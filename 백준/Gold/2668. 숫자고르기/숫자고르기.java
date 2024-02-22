1e8e665ebf37191aaba47c47ddf4ed917d68d7b2
Lee jinwoo <leejinwoo1126@gmail.com>
2024-02-22 15:22:31 +0900

    [Gold V] Title: 숫자고르기, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -3,64 +3,107 @@ import java.io.*;
 
 public class Main {
     
-     static StringBuilder sb = new StringBuilder();
-
+   static StringBuilder sb = new StringBuilder();
+    static InputProcessor inputProcessor = new InputProcessor();
     static int N;
+    static int[] NUMBERS;
+    static boolean[] VISIT, FINISH;
+    static List<Integer> RESULT = new ArrayList<>();
 
-    static int[] arr;
-
-    static boolean[] visit, finish;
-
-    static List<Integer> result = new ArrayList<>();
-
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-
-        arr = new int[N + 1];
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        visit = new boolean[N + 1];
-        finish = new boolean[N + 1];
-    }
-
-    static void dfs(int x) {
-        visit[x] = true;
-
-        int y = arr[x];
-        if(!visit[y]) {
-            dfs(y);
-        } else if(!finish[y]) {
-            while(x != y) {
-                result.add(y);
-                y = arr[y];
-            }
-            result.add(x);
-        }
-
-        finish[x] = true;
-    }
-
-    static void pro() {
-        for(int i = 1; i <= N; i++) {
-            if(!visit[i]) dfs(i);
-        }
-
-        Collections.sort(result);
-        sb.append(result.size()).append("\n");
-        for(int i : result) sb.append(i).append("\n");
-
-        System.out.println(sb);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
+    }
+
+    private static void input() {
+        N = inputProcessor.nextInt();
+        NUMBERS = new int[N + 1];
+        for(int i = 1; i <= N; i++) {
+            NUMBERS[i] = inputProcessor.nextInt();
+        }
+
+        VISIT = new boolean[N + 1];
+        FINISH = new boolean[N + 1];
+    }
+
+    public static void pro() {
+        for(int i = 1; i <= N; i++) {
+            if(!FINISH[i]) {
+                dfs(i);
+            }
+        }
+
+        Collections.sort(RESULT);
+        sb.append(RESULT.size()).append("\n");
+        for(int v : RESULT) {
+            sb.append(v).append("\n");
+        }
+    }
+
+    private static void dfs(int node) {
+        VISIT[node] = true;
+
+        int next = NUMBERS[node];
+        if(!VISIT[next]) {
+            dfs(next);
+        } else if(!FINISH[next]) {
+            while(node != next) {
+                RESULT.add(next);
+                next = NUMBERS[next];
+            }
+            RESULT.add(node);
+        }
+
+        FINISH[node] = true;
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String input = "";
+
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return input;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
     }
     
 }
