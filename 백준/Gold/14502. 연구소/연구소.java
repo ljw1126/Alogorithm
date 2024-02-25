71b6015e5ed6b9cc8865d51696de9148ea69356b
Lee jinwoo <leejinwoo1126@gmail.com>
2024-02-25 22:07:52 +0900

    [Gold IV] Title: 연구소, Time: 556 ms, Memory: 42776 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,167 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+     static StringBuilder sb = new StringBuilder();
+    static int[][] DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int[][] MATRIX;
+    static boolean[][] VISIT;
+    static int N, M, RESULT, BLANK_COUNT;
+    static int MAX_WALL_COUNT = 3;
+
+    static int[][] BLANKS;
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        pro();
+
+        output();
+    }
+
+    private static void pro() {
+        checkBlank();
+        makeWall(0, 0);
+
+        sb.append(RESULT);
+    }
+
+    private static void checkBlank() {
+        BLANKS = new int[65][2];
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                if(MATRIX[i][j] == 0) {
+                    BLANKS[BLANK_COUNT][0] = i;
+                    BLANKS[BLANK_COUNT][1] = j;
+                    BLANK_COUNT += 1;
+                }
+            }
+        }
+    }
+
+    private static void makeWall(int cnt, int idx) {
+        if(cnt == MAX_WALL_COUNT) {
+            spreadVirus();
+            return;
+        }
+
+        if(idx >= BLANK_COUNT) return;
+
+        // 벽을 세운다
+        MATRIX[BLANKS[idx][0]][BLANKS[idx][1]] = 1;
+        makeWall(cnt + 1, idx + 1);
+
+        // 벽을 세우지 않는다
+        MATRIX[BLANKS[idx][0]][BLANKS[idx][1]] = 0;
+        makeWall(cnt, idx + 1);
+    }
+
+    private static void spreadVirus() {
+        Deque<Integer> que = new ArrayDeque<>();
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                VISIT[i][j] = false;
+                if(MATRIX[i][j] == 2) {
+                    que.add(i);
+                    que.add(j);
+                    VISIT[i][j] = true;
+                }
+            }
+        }
+
+        while(!que.isEmpty()) {
+            int x = que.poll();
+            int y = que.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nx = x + DIR[i][0];
+                int ny = y + DIR[i][1];
+
+                if(nx < 1 || ny < 1 || nx > N || ny > M) continue;
+                if(VISIT[nx][ny]) continue;
+                if(MATRIX[nx][ny] != 0) continue;
+
+                que.add(nx);
+                que.add(ny);
+                VISIT[nx][ny] = true;
+            }
+        }
+
+        int count = 0;
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                if(MATRIX[i][j] == 0 && !VISIT[i][j]) { // 바이러스가 방문하지 않은 경우
+                    count += 1;
+                }
+            }
+        }
+
+        RESULT = Math.max(RESULT, count);
+    }
+
+    private static void input() {
+        InputProcessor inputProcessor = new InputProcessor();
+        N = inputProcessor.nextInt();
+        M = inputProcessor.nextInt();
+
+        MATRIX = new int[N + 1][M + 1];
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                MATRIX[i][j] = inputProcessor.nextInt();
+            }
+        }
+
+        VISIT = new boolean[N + 1][M + 1];
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static class InputProcessor {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String input = "";
+
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return input;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+    
+}
