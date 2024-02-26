e5ca3bf6193ec3f1d499771c4cbc6d2837efd932
Lee jinwoo <leejinwoo1126@gmail.com>
2024-02-26 18:50:14 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int T, N;
+
+    static int[] DP;
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        while(T > 0) {
+            T -= 1;
+
+            N = Integer.parseInt(br.readLine());
+            
+            if(N == 0) sb.append(1).append(" ").append(0).append("\n");
+            else sb.append(DP[N - 1]).append(" ").append(DP[N]).append("\n");
+        }
+    }
+
+    static void fibonacci() {
+        DP = new int[41];
+        DP[0] = 0;
+        DP[1] = 1;
+        for(int i = 2; i <= 40; i++) {
+            DP[i] = DP[i - 1] + DP[i - 2];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        fibonacci();
+        input();
+        System.out.println(sb);
+    }
+    
+}
