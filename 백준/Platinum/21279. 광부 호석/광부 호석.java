fa96f770249d9557eadce2c32fbacb691de59038
Lee jinwoo <leejinwoo1126@gmail.com>
2024-03-02 21:08:56 +0900

    [Platinum V] Title: 광부 호석, Time: 608 ms, Memory: 97092 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.java
+++ 백준/Platinum/21279. 광부 호석/광부 호석.java
@@ -3,98 +3,110 @@ import java.io.*;
 
 public class Main {
     
-    static int N, C;
+    static PrintWriter out = new PrintWriter(System.out);
 
-    static List<Stone>[] X, Y;
 
-    static int CNT;
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+        int n, c;
+        n = scan.nextInt();
+        c = scan.nextInt();
+        ArrayList<pair>[] xx = new ArrayList[100001];
+        ArrayList<pair>[] yy = new ArrayList[100001];
+        for(int i = 0; i < 100001; i++){
+            xx[i] = new ArrayList<pair>();
+            yy[i] = new ArrayList<pair>();
+        }
+        for(int i = 0; i < n; i++){
+            long x, y, v;
+            x = scan.nextLong();
+            y = scan.nextLong();
+            v = scan.nextLong();
+            xx[(int)x].add(new pair(y,v));
+            yy[(int)y].add(new pair(x,v));
+        }
+        long mx = 0;
+        long curval = 0;
+        int curnum = 0;
+        int xlim = 0;
+        int ylim = 100000;
 
-    static long SUM;
+        while(xlim <= 100000){
+            for(int i = 0; i < xx[xlim].size(); i++){
+                if(xx[xlim].get(i).x > ylim) continue;
+                curval += xx[xlim].get(i).y;
+                curnum++;
+            }
+            while(curnum > c){
+                for(int i = 0; i < yy[ylim].size(); i++){
+                    if(yy[ylim].get(i).x > xlim) continue;
+                    curval -= yy[ylim].get(i).y;
+                    curnum--;
+                }
+                ylim--;
+            }
+            xlim++;
+            if(mx < curval) mx = curval;
+        }
+        System.out.println(mx);
+    }
 
-    static class Stone {
-        int x;
-        int y;
-        int v;
 
-        public Stone(int x, int y, int v) {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class pair{
+        public long x, y;
+        pair(long x, long y){
             this.x = x;
             this.y = y;
-            this.v = v;
+        }
+        pair(){
+            this.x = 0;
+            this.y = 0;
         }
     }
-
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); // 광물의 개수
-        C = Integer.parseInt(st.nextToken()); // 호석이가 가진 돈
-
-        X = new ArrayList[100002];
-        Y = new ArrayList[100002];
-        for(int i = 0; i <= 100001; i++) {
-            X[i] = new ArrayList<>();
-            Y[i] = new ArrayList<>();
-        }
-
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            X[x].add(new Stone(x, y, v)); // 너비 기준
-            Y[y].add(new Stone(x, y, v)); // 높이 기준
-        }
-    }
-
-    static void del(int x, int y) {
-        for(Stone stone : Y[y]) {
-            if(stone.x <= x) {
-                CNT -= 1;
-                SUM -= stone.v;
-            }
-        }
-    }
-
-    static void add(int x, int y) {
-        for(Stone stone : X[x]) {
-            if(stone.y <= y) {
-                CNT += 1;
-                SUM += stone.v;
-            }
-        }
-    }
-
-    static void pro() {
-        long ans = 0L;
-        int W = -1;
-        int H = 100001;
-
-        CNT = 0;
-        SUM = 0L;
-
-        while(W < 100000 && H > 0) {
-            if(CNT > C) {
-                H -= 1; 
-                del(W, H);
-            } else {
-                W += 1;
-                add(W, H);
-            }
-
-            if(CNT <= C) {
-                ans = Math.max(ans, SUM);
-            }
-        }
-
-        System.out.println(ans * 1L);
-    }
-
-    public static void main(String[] args) throws Exception {
-        input();
-        pro();
-    }
-    
 }
