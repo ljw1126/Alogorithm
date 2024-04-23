c7d603a169bea0807f9625e3c9863d1b90989ff2
Lee jinwoo <leejinwoo1126@gmail.com>
2024-04-23 18:39:52 +0900

    [Gold V] Title: 수 고르기, Time: 400 ms, Memory: 32640 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -2,86 +2,87 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static FastReader scan = new FastReader();
+    
+   private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-    static int N, M;
-    static int[] a;
+    private static int N, M;
+    private static int[] A;
 
-    static void input() {
-        N = scan.nextInt();
-        M = scan.nextInt();
-        a = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            a[i] = scan.nextInt();
-        }
-    }
-
-    static void pro() {
-        Arrays.sort(a,1,N+1);
-        int R = 1, ans = Integer.MAX_VALUE;
-        
-        for (int L = 1; L <= N; L++) {
-            while(R+1 <= N && a[R] - a[L] < M){
-                R++;
-            }
-         
-            if(a[R] - a[L] >= M){
-                 ans = Math.min(ans, a[R] - a[L]);
-            }
-        }
-
-        System.out.println(ans);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
     }
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+    private static void input() {
+        N = inputProcessor.nextInt();
+        M = inputProcessor.nextInt();
 
-        public FastReader() {
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = inputProcessor.nextInt();
+        }
+    }
+
+    private static void pro() {
+        Arrays.sort(A, 1, N + 1);
+
+        int result = Integer.MAX_VALUE;
+        for (int L = 1, R = 1; L <= N; L++) {
+            while (R + 1 <= N && A[R] - A[L] < M) {
+                R += 1;
+            }
+
+            if (A[R] - A[L] >= M) {
+                result = Math.min(result, A[R] - A[L]);
+            }
+        }
+
+        sb.append(result);
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
+        public String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt() {
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public int nextInt() {
             return Integer.parseInt(next());
         }
 
-        long nextLong() {
+        public long nextLong() {
             return Long.parseLong(next());
         }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
+    
 }
