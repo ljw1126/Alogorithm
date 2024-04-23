d568dd4d1207b57662d055e53f558d4378ef0b1c
Lee jinwoo <leejinwoo1126@gmail.com>
2024-04-23 15:12:40 +0900

    [Silver II] Title: 나무 자르기, Time: 520 ms, Memory: 119524 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt(); 
+        M = scan.nextInt(); 
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = scan.nextInt();
+        }
+    }
+
+    static boolean determination(int H) {
+         long sum = 0 ; 
+        for(int i=1; i<=N;i++){
+            if(A[i] > H){
+                sum += A[i] - H;
+            }
+        }
+        return sum >= M; 
+    }
+
+    static void pro() {
+        long L = 0, R = 2000000000, ans = 0;
+        while(L <= R){
+            long mid = (L+R)/2;
+            if(determination((int)mid)){
+                ans = mid; 
+                L = mid + 1;
+            }else{
+                R = mid - 1;
+            }
+        }
+        
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
