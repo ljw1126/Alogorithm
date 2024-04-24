a75ca21906e59c82f622999e223375988344d512
Lee jinwoo <leejinwoo1126@gmail.com>
2024-04-24 19:40:38 +0900

    [Silver II] Title: 결혼식, Time: 204 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -0,0 +1,115 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static List<Integer>[] ADJ;
+    static int[] DIST;
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        pro();
+
+        output();
+    }
+
+    private static void pro() {
+        bfs(1);
+
+        int count = 0;
+        for(int i = 2; i <= N; i++) {
+            if(DIST[i] != -1 && DIST[i] <= 2) count += 1;
+        }
+
+        sb.append(count);
+    }
+
+    private static void bfs(int start) {
+        Deque<Integer> que = new ArrayDeque<>();
+        que.add(start);
+
+        Arrays.fill(DIST, -1);
+        DIST[start] = 0;
+
+        while(!que.isEmpty()) {
+            int cur = que.poll();
+
+            for(int next : ADJ[cur]) {
+                if(DIST[next] != -1) continue;
+
+                DIST[next] = DIST[cur] + 1;
+                que.add(next);
+            }
+        }
+    }
+
+    private static void input() {
+        InputProcessor inputProcessor = new InputProcessor();
+        N = inputProcessor.nextInt(); // 동기의 수
+        M = inputProcessor.nextInt(); // 리스트 길이(간선)
+
+        ADJ = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) {
+            ADJ[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i <= M; i++) {
+            int from = inputProcessor.nextInt();
+            int to = inputProcessor.nextInt();
+
+            ADJ[from].add(to);
+            ADJ[to].add(from);
+        }
+
+        DIST = new int[N + 1];
+    }
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String input = "";
+
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return input;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
