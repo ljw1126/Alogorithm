83583ee3f09bfee2e90ccfb2c0c5ad77ccec5d9b
Lee jinwoo <leejinwoo1126@gmail.com>
2024-04-25 13:35:36 +0900

    [Silver I] Title: 부동산 다툼, Time: 528 ms, Memory: 41460 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
@@ -5,95 +5,50 @@ public class Main {
     
 static StringBuilder sb = new StringBuilder();
 
-    static StringBuilder sb = new StringBuilder();
-    static InputProcessor inputProcessor = new InputProcessor();
     static int N, Q;
-    static boolean[] VISIT;
 
-    public static void main(String[] args) throws IOException {
-        input();
+    static boolean[] REAL_ESTATE;
 
-        pro();
+    static List<Integer> queries = new ArrayList<>();
 
-        output();
-    }
-    private static void input() {
-        N = inputProcessor.nextInt(); // 땅의 개수(노드)
-        Q = inputProcessor.nextInt(); // 오리 수(쿼리)
-        VISIT = new boolean[N + 1];
-    }
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    private static void pro() {
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        REAL_ESTATE = new boolean[N + 1];
+
         for(int i = 1; i <= Q; i++) {
-            int duck = inputProcessor.nextInt();
-
-            int result = dfs(duck, 0);
-
-            if(result == 0) {
-                VISIT[duck] = true;
-            }
-
-            sb.append(result).append("\n");
+            queries.add(Integer.parseInt(br.readLine()));
         }
     }
 
-    // 예외. 자기 자신이 점유되고 있는 경우 1 1 1 => 0 1 1
-    private static int dfs(int node, int occ) {
-        if(node < 1) {
-            return occ;
-        }
-
-        if(VISIT[node]) {
-            occ = node;
-        }
-
-        return dfs(node >> 1, occ);
-    }
-
-    private static void output() throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while(st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String input = "";
-
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
+    static void execute(int q) {
+        int idx = q;
+        int ans = 0;
+        while(idx != 0) {
+            if(REAL_ESTATE[idx]) {
+                ans = idx;
             }
 
-            return input;
+            idx /= 2;
         }
 
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
+        if(ans == 0) REAL_ESTATE[q] = true;
 
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
+        sb.append(ans).append("\n");
+    }
+
+    static void pro() {
+        for(int query : queries) execute(query);
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        pro();
     }
 }
