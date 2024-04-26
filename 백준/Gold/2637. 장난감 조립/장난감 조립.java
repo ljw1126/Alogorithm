aef29565845b5f9df4a55a3c9e16d9d5867937b0
Lee jinwoo <leejinwoo1126@gmail.com>
2024-04-26 21:32:58 +0900

    [Gold II] Title: 장난감 조립, Time: 136 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.java
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.java
@@ -1,124 +1,116 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     
-    static StringBuilder sb = new StringBuilder();
-    static InputProcessor inputProcessor = new InputProcessor();
-    static String RESULT_FORMAT = "%d %d\n";
-    static int N, M;
-    static List<Node>[] ADJ;
-    static int[] IN_DEGREE;
-    static Material[] MATERIAL_ARR;
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    private static int N, M;
+    private static List<List<Node>> ADJ;
+    private static int[] IN_DEGREE;
 
     public static void main(String[] args) throws IOException {
         input();
-
         pro();
-
         output();
     }
 
-    private static void pro() {
-        bfs();
+    private static void input() {
+        N = inputProcessor.nextInt(); // 부품 번호 수
+        M = inputProcessor.nextInt(); // 관계 수
 
-        Material material = MATERIAL_ARR[N];
-        Set<Integer> keys = material.keySet();
-        for(int key : keys) {
-            sb.append(String.format(RESULT_FORMAT, key, material.supply.get(key)));
-        }
-    }
-
-    private static void bfs() {
-        Deque<Integer> que = new ArrayDeque<>();
-
-        Set<Integer> basic = new HashSet<>();
-        for(int i = 1; i <= N; i++) {
-            if(IN_DEGREE[i] == 0) {
-                que.add(i);
-                basic.add(i);
-            }
+        ADJ = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            ADJ.add(new ArrayList<>());
         }
 
-        while(!que.isEmpty()) {
-            int cur = que.poll();
+        IN_DEGREE = new int[N + 1];
+        for (int i = 1; i <= M; i++) {
+            int x = inputProcessor.nextInt(); // x를 만드는데
+            int y = inputProcessor.nextInt(); // 부품 y가
+            int k = inputProcessor.nextInt(); // k개 필요하다
 
-            for(Node next : ADJ[cur]) {
-                if(basic.contains(cur)) {
-                    MATERIAL_ARR[next.to].supply(cur, next.amount);
-                } else {
-                    MATERIAL_ARR[next.to].supply(MATERIAL_ARR[cur], next.amount);
-                }
-                
-                IN_DEGREE[next.to] -= 1;
-                if(IN_DEGREE[next.to] == 0) {
-                    que.add(next.to);
-                }
-            }
-        }
-    }
-
-    private static class Material {
-        Map<Integer, Integer> supply;
-
-        public Material() {
-            supply = new TreeMap<>();
-        }
-
-        public Set<Integer> keySet() {
-            return supply.keySet();
-        }
-
-        public int get(int key) {
-            return supply.get(key);
-        }
-        
-        public void supply(int no, int amount) {
-            supply.put(no, supply.getOrDefault(no, 0) + amount);
-        }
-
-        public void supply(Material material, int amount) {
-            Set<Integer> keys = material.keySet();
-            for(int key : keys) {
-                supply.put(key, supply.getOrDefault(key, 0) + material.get(key) * amount);
-            }
+            ADJ.get(y).add(new Node(x, k)); // x를 만드는데 y가 k개 필요
+            IN_DEGREE[x] += 1;
         }
     }
 
     private static class Node {
-        int to;
-        int amount;
-        public Node(int to, int amount) {
-            this.to = to;
+        private int idx;
+        private int amount;
+
+        public Node(int idx, int amount) {
+            this.idx = idx;
             this.amount = amount;
         }
     }
 
-    private static void input() {
-        N = inputProcessor.nextInt();
-        M = inputProcessor.nextInt();
+    private static class Material {
+        private Map<Integer, Integer> partsAndCountMap = new HashMap<>();
 
-
-        ADJ = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) {
-            ADJ[i] = new ArrayList<>();
+        public void supply(int basicParts, int amount) {
+            partsAndCountMap.put(basicParts, this.get(basicParts) + amount);
         }
 
-        IN_DEGREE = new int[N + 1];
-        for(int i = 1; i <= M; i++) {
-            int x = inputProcessor.nextInt();
-            int y = inputProcessor.nextInt();
-            int k = inputProcessor.nextInt();
-
-            ADJ[y].add(new Node(x, k)); 
-            IN_DEGREE[x] += 1;
+        public void supply(Material other, int amount) {
+            Set<Integer> keySet = other.keySet();
+            for (int key : keySet) {
+                partsAndCountMap.put(key, this.get(key) + (other.get(key) * amount));
+            }
         }
 
-        MATERIAL_ARR = new Material[N + 1];
-        for(int i = 1; i <= N; i++) {
-            MATERIAL_ARR[i] = new Material();
+        public int get(int key) {
+            return partsAndCountMap.getOrDefault(key, 0);
+        }
+
+        public Set<Integer> keySet() {
+            return partsAndCountMap.keySet();
         }
     }
+
+    private static void pro() {
+        Deque<Integer> que = new ArrayDeque<>();
+
+        Set<Integer> basicParts = new HashSet<>();
+        for (int i = 1; i <= N; i++) {
+            if (IN_DEGREE[i] == 0) {
+                que.add(i);
+                basicParts.add(i);
+            }
+        }
+
+        // 각 중간 부품별로 필요한 파트 카운팅
+        Material[] materials = new Material[N + 1];
+        for (int i = 0; i <= N; i++) {
+            materials[i] = new Material();
+        }
+
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+
+            for (Node next : ADJ.get(cur)) { // next를 만드는데 cur이 amount만큼 필요하다
+                IN_DEGREE[next.idx] -= 1;
+                if (IN_DEGREE[next.idx] == 0) {
+                    que.add(next.idx);
+                }
+
+                if (basicParts.contains(cur)) { // 중간 부품
+                    materials[next.idx].supply(cur, next.amount);
+                } else {
+                    materials[next.idx].supply(materials[cur], next.amount);
+                }
+            }
+        }
+
+        Material result = materials[N];
+        result.keySet()
+                .stream()
+                .sorted()
+                .forEach(key -> sb.append(key).append(" ").append(result.get(key)).append("\n"));
+
+    }
+
 
     private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -128,15 +120,15 @@ public class Main {
     }
 
     private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
+        private BufferedReader br;
+        private StringTokenizer st;
 
         public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         public String next() {
-            while(st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
@@ -149,7 +141,6 @@ public class Main {
 
         public String nextLine() {
             String input = "";
-
             try {
                 input = br.readLine();
             } catch (IOException e) {
