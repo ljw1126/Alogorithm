edce0806709c25f6d36bcf7bee082809b2a5cefd
Lee jinwoo <leejinwoo1126@gmail.com>
2024-05-03 16:55:20 +0900

    [Silver I] Title: 동물원, Time: 132 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -3,30 +3,82 @@ import java.io.*;
 
 public class Main {
     
-    static int N;
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-    static int MOD = 9901;
-    static int[] DP;
+    private static final int MOD = 9901;
+    private static int N;
+    private static int[] DP;
 
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
     }
 
-    static void pro() {
-        DP = new int[100001];
+    private static void input() {
+        N = inputProcessor.nextInt();
+        DP = new int[N + 1];
+    }
+
+    private static void pro() {
         DP[0] = 1;
         DP[1] = 3;
 
-        for(int i = 2; i <= 100000; i++) {
-            DP[i] = (2 * DP[i - 1] + DP[i - 2]) % MOD;
+        for (int i = 2; i <= N; i++) {
+            DP[i] = 2 * DP[i - 1] + DP[i - 2];
+            DP[i] %= MOD;
         }
+
+        sb.append(DP[N]);
     }
 
-    public static void main(String[] args) throws Exception {
-        input();
-        pro();
-        System.out.println(DP[N]);
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String input = "";
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return input;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
     }
     
 }
