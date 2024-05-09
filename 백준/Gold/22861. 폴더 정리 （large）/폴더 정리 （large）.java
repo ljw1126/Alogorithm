7b34167c394a3f9af98e103482ddb607cd3ba777
Lee jinwoo <leejinwoo1126@gmail.com>
2024-05-09 13:15:34 +0900

    [Gold I] Title: 폴더 정리 (large), Time: 848 ms, Memory: 98108 KB -BaekjoonHub

--- 백준/Gold/22861. 폴더 정리 （large）/폴더 정리 （large）.java
+++ 백준/Gold/22861. 폴더 정리 （large）/폴더 정리 （large）.java
@@ -3,11 +3,57 @@ import java.io.*;
 
 public class Main {
     
-    private static StringBuilder sb = new StringBuilder();
+     private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
-    private static int N, M, K, Q;
-    private static Map<String, Folder> DIRECTORY = new HashMap<>();
+    private static Map<String, Folder> HIERARCHY = new HashMap<>();
+
+    private static class Folder {
+        private Set<Folder> childs = new HashSet<>();
+        private Set<String> files = new HashSet<>();
+        private int fileCount;
+        private String name;
+
+        public Folder(String name) {
+            this.name = name;
+        }
+
+        public void addFile(String fileName) {
+            files.add(fileName);
+            fileCount += 1;
+        }
+
+        public void addFolder(Folder folder) {
+            childs.add(folder);
+        }
+
+        public void merge(Folder other) {
+            files.addAll(other.files);
+            fileCount += other.fileCount;
+        }
+
+        public void move(Folder from) {
+            childs.addAll(from.childs);
+            files.addAll(from.files);
+            fileCount = files.size();
+        }
+
+        public int uniqueFileSize() {
+            return this.files.size();
+        }
+
+        public int fileCount() {
+            return this.fileCount;
+        }
+
+        public boolean sameName(String other) {
+            return name.equals(other);
+        }
+
+        public void removeFolder(Folder other) {
+            childs.remove(other);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         input();
@@ -16,83 +62,68 @@ public class Main {
     }
 
     private static void input() {
-        N = inputProcessor.nextInt(); // 폴더의 총 개수
-        M = inputProcessor.nextInt(); // 파일의 총 개수
+        int n = inputProcessor.nextInt(); // 폴더의 총 개수
+        int m = inputProcessor.nextInt(); // 파일의 총 개수
 
-        for (int i = 1; i <= N + M; i++) {
-            String parent = inputProcessor.next(); // 상위 폴더 명
-            String child = inputProcessor.next(); // 폴더 또는 파일명
+        for (int i = 1; i <= n + m; i++) {
+            String p = inputProcessor.next(); // 상위 폴더의 이름
+            String f = inputProcessor.next(); // 폴더 또는 파일 이름
             int c = inputProcessor.nextInt(); // 폴더 1, 파일 0
 
-            Folder dir = findFolder(parent);
-            if (c == 1) { // 폴더
-                Folder other = findFolder(child);
-                dir.addFolder(other);
-            } else { // 파일
-                dir.addFile(child);
+            Folder parent = findFolder(p);
+            if (c == 0) { // 파일
+                parent.addFile(f);
+            } else { // 폴더
+                Folder child = findFolder(f);
+                parent.addFolder(child);
             }
+        }
+
+        int k = inputProcessor.nextInt(); // 옮기는 횟수
+        for (int i = 1; i <= k; i++) {
+            String from = inputProcessor.next();
+            String to = inputProcessor.next();
+
+            Folder fromDir = parseAndFindFolder(from);
+            Folder toDir = parseAndFindFolder(to);
+
+            toDir.move(fromDir);
+            erase(fromDir);
+        }
+    }
+
+    private static Folder parseAndFindFolder(String path) {
+        int idx = path.lastIndexOf("/");
+        String dirName = path.substring(idx + 1);
+        return HIERARCHY.get(dirName);
+    }
+
+    private static void erase(Folder target) {
+        Set<String> keySet = HIERARCHY.keySet();
+        for (String dir : keySet) {
+            if (target.sameName(dir)) continue;
+
+            Folder folder = HIERARCHY.get(dir);
+            folder.removeFolder(target);
         }
     }
 
     private static Folder findFolder(String name) {
-        if (DIRECTORY.containsKey(name)) {
-            return DIRECTORY.get(name);
-        }
+        if (HIERARCHY.containsKey(name)) return HIERARCHY.get(name);
 
-        Folder newFolder = new Folder(name);
-        DIRECTORY.put(name, newFolder);
-        return newFolder;
+        Folder created = new Folder(name);
+        HIERARCHY.put(name, created);
+        return created;
     }
 
-    private static class Folder {
-        private Set<Folder> childs = new HashSet<>();
-        private Set<String> fildNames = new HashSet<>();
-        private int fileCount;
-        private String name;
+    private static void pro() {
+        dfs(findFolder("main"));
 
-        public Folder(String name) {
-            this.name = name;
-        }
-
-        public void addFolder(Folder folder) {
-            childs.add(folder);
-        }
-
-        public void addFile(String fileName) {
-            fildNames.add(fileName);
-            fileCount += 1;
-        }
-
-        public void merge(Folder other) {
-            fildNames.addAll(other.fildNames);
-            fileCount += other.fileCount;
-        }
-
-        public void move(Folder other) {
-            this.childs.addAll(other.childs);
-            this.fildNames.addAll(other.fildNames);
-            this.fileCount = this.fildNames.size();
-        }
-
-        public int size() {
-            return this.fildNames.size();
-        }
-
-        public void removeFolder(Folder other) {
-            childs.remove(other);
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if (this == other) return true;
-            if (other == null || getClass() != other.getClass()) return false;
-            Folder folder = (Folder) other;
-            return Objects.equals(name, folder.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name);
+        int query = inputProcessor.nextInt();
+        for (int i = 1; i <= query; i++) {
+            String path = inputProcessor.next(); // 경로 정보
+            Folder folder = parseAndFindFolder(path);
+            sb.append(folder.uniqueFileSize()).append(" ").append(folder.fileCount()).append("\n");
         }
     }
 
@@ -101,46 +132,6 @@ public class Main {
             dfs(child);
             folder.merge(child);
         }
-    }
-
-    private static void pro() {
-        K = inputProcessor.nextInt(); // 옮기는 횟수
-        for (int i = 1; i <= K; i++) {
-            String from = inputProcessor.next();
-            String to = inputProcessor.next();
-
-            moveDirectory(lastDirectoryName(from), lastDirectoryName(to));
-        }
-
-        dfs(DIRECTORY.get("main"));
-
-        Q = inputProcessor.nextInt(); // 쿼리 수
-        for (int i = 1; i <= Q; i++) {
-            String path = inputProcessor.next();
-            String folderName = lastDirectoryName(path);
-            Folder folder = DIRECTORY.get(folderName);
-            sb.append(folder.size()).append(" ").append(folder.fileCount).append("\n");
-        }
-    }
-
-    private static void moveDirectory(String from, String to) {
-        Folder fromFolder = DIRECTORY.get(from);
-        Folder toFolder = DIRECTORY.get(to);
-        toFolder.move(fromFolder); // b -> a
-
-        Set<String> keySet = DIRECTORY.keySet();
-        for (String dir : keySet) {
-            if (dir.equals(from)) continue;
-
-            Folder folder = DIRECTORY.get(dir);
-            folder.removeFolder(fromFolder);
-        }
-    }
-
-    private static String lastDirectoryName(String path) {
-        int idx = path.lastIndexOf("/");
-        String folderName = path.substring(idx + 1);
-        return folderName;
     }
 
     private static void output() throws IOException {
