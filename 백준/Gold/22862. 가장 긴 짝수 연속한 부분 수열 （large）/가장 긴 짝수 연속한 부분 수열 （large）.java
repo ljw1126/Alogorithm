a5850af4cc2c03d2b756eaef2a2db0dcac1d15ff
Lee jinwoo <leejinwoo1126@gmail.com>
2024-05-11 21:50:59 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 520 ms, Memory: 91292 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
     private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
     private static int N, K;
-    private static int[] S;
+    private static int[] DATA;
 
 
     public static void main(String[] args) throws IOException {
@@ -20,25 +19,27 @@ public class Main {
         N = inputProcessor.nextInt(); // 길이
         K = inputProcessor.nextInt(); // 최대 횟수
 
-        S = new int[N + 1];
+        DATA = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            S[i] = inputProcessor.nextInt();
+            DATA[i] = inputProcessor.nextInt();
         }
     }
 
     private static void pro() {
-        int cnt = 0;
+
         int result = 0;
+        int cnt = 0;
         for (int L = 1, R = 0; L <= N; L++) {
             while (R + 1 <= N && cnt <= K) {
                 R += 1;
-                if (S[R] % 2 == 1) { // 홀수 인 경우
+                if (DATA[R] % 2 == 1) {
                     cnt += 1;
                 }
             }
 
-            result = Math.max(result, R - L + 1 - cnt);
-            if (S[L] % 2 == 1) {
+            result = Math.max(result, R - L - cnt + 1);
+
+            if (DATA[L] % 2 == 1) {
                 cnt -= 1;
             }
         }
@@ -93,5 +94,4 @@ public class Main {
         }
 
     }
-    
 }
