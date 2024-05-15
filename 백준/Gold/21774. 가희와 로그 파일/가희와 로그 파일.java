d12599d3d9f67e17f20a9671732fa16b32c1462c
Lee jinwoo <leejinwoo1126@gmail.com>
2024-05-15 19:28:13 +0900

    [Gold III] Title: 가희와 로그 파일, Time: 1676 ms, Memory: 159412 KB -BaekjoonHub

--- 백준/Gold/21774. 가희와 로그 파일/가희와 로그 파일.java
+++ 백준/Gold/21774. 가희와 로그 파일/가희와 로그 파일.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class Main {
     
-  private static StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
-    private static final String SHARP_DELIMITER = "\\#";
-    private static int N, Q;
-    private static List<List<String>> DATA;
+    private static final String SHARP = "#";
 
+    private static int N, Q;
+    private static List<List<String>> LOGS;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -21,57 +21,57 @@ public class Main {
         N = inputProcessor.nextInt();
         Q = inputProcessor.nextInt();
 
-        DATA = new ArrayList<>();
+        LOGS = new ArrayList<>();
         for (int i = 0; i <= 6; i++) {
-            DATA.add(new ArrayList<>());
+            LOGS.add(new ArrayList<>());
         }
 
         for (int i = 1; i <= N; i++) {
-            String text = inputProcessor.nextLine();
-            String[] tokens = text.split(SHARP_DELIMITER);
+            String line = inputProcessor.nextLine();
+            String[] tokens = line.split(SHARP);
+
             String date = tokens[0];
             int level = Integer.parseInt(tokens[1]);
 
-            DATA.get(level).add(date);
+            LOGS.get(level).add(date);
         }
     }
 
     private static void pro() {
         for (int i = 1; i <= Q; i++) {
-            String text = inputProcessor.nextLine();
-            String[] tokens = text.split(SHARP_DELIMITER);
+            String query = inputProcessor.nextLine();
+            String[] tokens = query.split(SHARP);
 
             String startDate = tokens[0];
             String endDate = tokens[1];
             int level = Integer.parseInt(tokens[2]);
 
             int count = 0;
-            for (int j = level; j <= 6; j++) {
-                count += countByRange(DATA.get(j), startDate, endDate);
+            for (int l = level; l <= 6; l++) {
+                count += countByRange(LOGS.get(l), startDate, endDate);
             }
 
             sb.append(count).append("\n");
         }
     }
 
-    private static int countByRange(List<String> dates, String startDate, String endDate) {
-        if (dates.isEmpty()) {
-            return 0;
-        }
+    private static int countByRange(List<String> logs, String startDate, String endDate) {
+        if (logs.isEmpty()) return 0;
 
-        int lower = lowerBound(dates, 0, dates.size(), startDate);
-        int upper = upperBound(dates, 0, dates.size(), endDate);
+        int lower = lowerBound(logs, 0, logs.size(), startDate);
+        int upper = upperBound(logs, 0, logs.size(), endDate);
+
         return upper - lower;
     }
 
-    private static int lowerBound(List<String> dates, int start, int end, String startDate) {
+    private static int lowerBound(List<String> logs, int start, int end, String startDate) {
         int L = start;
         int R = end;
 
         while (L < R) {
             int mid = (L + R) / 2;
 
-            if (dates.get(mid).compareTo(startDate) < 0) {
+            if (logs.get(mid).compareTo(startDate) < 0) {
                 L = mid + 1;
             } else {
                 R = mid;
@@ -81,14 +81,14 @@ public class Main {
         return L;
     }
 
-    private static int upperBound(List<String> dates, int start, int end, String endDate) {
+    private static int upperBound(List<String> logs, int start, int end, String endDate) {
         int L = start;
         int R = end;
 
         while (L < R) {
             int mid = (L + R) / 2;
 
-            if (dates.get(mid).compareTo(endDate) <= 0) {
+            if (logs.get(mid).compareTo(endDate) <= 0) {
                 L = mid + 1;
             } else {
                 R = mid;
