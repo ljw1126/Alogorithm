45a9a2c252b34aece05fc1730279f0d082890f93
Lee jinwoo <leejinwoo1126@gmail.com>
2024-05-30 18:56:46 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
+    }
+
+    private static void input() {
+
+    }
+
+    private static void pro() {
+        while (true) {
+            String input = inputProcessor.nextLine();
+
+            if (input == null || input.isBlank()) break;
+
+            sb.append(input).append("\n");
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String input = "";
+
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return input;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+    
+}
