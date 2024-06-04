91f452981d0d548eb117f6b34acb660da765357f
Lee jinwoo <leejinwoo1126@gmail.com>
2024-06-04 22:47:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 4.39 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 3.58 ms
+메모리: 83.7 MB, 시간: 4.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 22:45:01
+2024년 06월 04일 22:47:25
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,12 +4,12 @@ import java.util.stream.IntStream;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         return IntStream.range(0, n)
-            .mapToObj(i -> toSecretMap(Long.valueOf(arr1[i] | arr2[i]), n))
+            .mapToObj(i -> toSecretMap(arr1[i] | arr2[i], n))
             .toArray(String[]::new);
     }
     
-    private String toSecretMap(long value, int n) {
-        String str = Long.toBinaryString(value);
+    private String toSecretMap(int value, int n) {
+        String str = Integer.toBinaryString(value);
         StringBuilder sb = new StringBuilder();
         
         if(n - str.length() > 0) {
