95ac722869ecc3813b395356dcc0cae6a6a4994c
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-03 23:59:16 +0900

    [Gold V] Title: 트리와 쿼리, Time: 700 ms, Memory: 78648 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -3,62 +3,56 @@ import java.io.*;
 
 public class Main {
     
-    static StringBuilder sb = new StringBuilder();
-
-    static int N, R, Q;
-    static List<Integer>[] ADJ;
-    static List<Integer> QUERY = new LinkedList<>();
-    static int[] SUBTREE;
-
+   private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
     public static void main(String[] args) throws IOException {
         input();
-
         pro();
-
         output();
     }
+
+    private static int N, R, Q;
+    private static List<List<Integer>> ADJ;
+    private static int[] SUBTREE;
+
     private static void input() {
-        InputProcessor inputProcessor = new InputProcessor();
-        N = inputProcessor.nextInt(); // 정점의 수
-        R = inputProcessor.nextInt(); // 루트의 번호
-        Q = inputProcessor.nextInt(); // 쿼리의 수
+        N = inputProcessor.nextInt(); // 정점
+        R = inputProcessor.nextInt(); // 루트 번호
+        Q = inputProcessor.nextInt(); // 쿼리수
+
+        ADJ = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            ADJ.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= N - 1; i++) {
+            int from = inputProcessor.nextInt();
+            int to = inputProcessor.nextInt();
+
+            ADJ.get(from).add(to);
+            ADJ.get(to).add(from);
+        }
 
         SUBTREE = new int[N + 1];
-
-        ADJ = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) {
-            ADJ[i] = new ArrayList<>();
-        }
-
-        for(int i = 1; i <= N - 1; i++) {
-            int u = inputProcessor.nextInt();
-            int v = inputProcessor.nextInt();
-
-            ADJ[u].add(v);
-            ADJ[v].add(u);
-        }
-
-        for(int i = 1; i <= Q; i++) {
-            QUERY.add(inputProcessor.nextInt());
-        }
     }
 
     private static void pro() {
         dfs(R, -1);
-
-        for(int q : QUERY) {
-            sb.append(SUBTREE[q]).append("\n");
+        for (int i = 1; i <= Q; i++) {
+            int node = inputProcessor.nextInt();
+            sb.append(SUBTREE[node]).append("\n");
         }
     }
 
-    private static void dfs(int node, int prev) {
+    private static void dfs(int node, int parent) {
         SUBTREE[node] = 1;
 
-        for(int child : ADJ[node]) {
-            if(child == prev) continue;
+        for (int child : ADJ.get(node)) {
+            if (child == parent) continue;
 
             dfs(child, node);
+
             SUBTREE[node] += SUBTREE[child];
         }
     }
@@ -71,34 +65,35 @@ public class Main {
     }
 
     private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
+        private BufferedReader br;
+        private StringTokenizer st;
 
         public InputProcessor() {
             this.br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         public String next() {
-            while(st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
             return st.nextToken();
         }
 
         public String nextLine() {
-            String input = "";
+            String result = "";
 
             try {
-                input = br.readLine();
+                result = br.readLine();
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
 
-            return input;
+            return result;
         }
 
         public int nextInt() {
