aac0ebe2aa91f12a8cfcf9ecce6f2252490a8cd6
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-03 12:45:52 +0900

    [Gold I] Title: 트리 디자이너 호석, Time: 656 ms, Memory: 74564 KB -BaekjoonHub

--- 백준/Gold/22253. 트리 디자이너 호석/트리 디자이너 호석.java
+++ 백준/Gold/22253. 트리 디자이너 호석/트리 디자이너 호석.java
@@ -3,78 +3,137 @@ import java.io.*;
 
 public class Main {
     
-    static int N;
+     private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-    static int[] V;
-
-    static int MOD = 1000000007;
-
-    static List<Integer>[] adj;
-
-    static int[][] dp;
-
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine()); // 정점의 개수
-
-        st = new StringTokenizer(br.readLine());
-        V = new int[N + 1];
-        for(int i = 1; i <= N; i++) {
-            V[i] = Integer.parseInt(st.nextToken());
-        }
-
-        adj = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
-
-        for(int i = 1; i <= N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-
-            adj[from].add(to);
-            adj[to].add(from);
-        }
-
-        dp = new int[N + 1][10]; // 0 ~ 9
-    }
-
-    static void dp(int idx, int prev) {
-        dp[idx][V[idx]] = 1;
-
-        for(int child : adj[idx]) {
-            if(child == prev) continue;
-
-            dp(child, idx);
-
-            for(int i = 0; i <= 9; i++) {
-                dp[idx][i] += dp[child][i];
-                dp[idx][i] %= MOD;
-            }
-
-            for(int i = V[idx]; i <= 9; i++) {
-                dp[idx][V[idx]] += dp[child][i];
-                dp[idx][V[idx]] %= MOD;
-            }
-        }
-    }
-
-    static void pro() {
-        dp(1, -1);
-
-        int ans = 0;
-        for(int i = 0; i <= 9; i++) {
-            ans += dp[1][i];
-            ans %= MOD;
-        }
-
-        System.out.println(ans);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
+    }
+
+    private static final int MOD = 1_000_000_007;
+
+    private static int N;
+
+    private static int[] POINT;
+    private static int[][] DP;
+    private static List<List<Integer>> ADJ;
+
+    // 각 정점의 숫자는 0 ~ 9 까지
+    private static void input() {
+        N = inputProcessor.nextInt();
+
+        ADJ = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            ADJ.add(new ArrayList<>());
+        }
+
+        // 1 ~ N 노드까지 0 ~ 9 사이의 숫자를 가진다
+        DP = new int[N + 1][10];
+        POINT = new int[N + 1];  // 0 ~ 9 까지 존재
+        for (int i = 1; i <= N; i++) {
+            POINT[i] = inputProcessor.nextInt();
+            DP[i][POINT[i]] = 1;
+        }
+
+        for (int i = 1; i <= N - 1; i++) {
+            int from = inputProcessor.nextInt();
+            int to = inputProcessor.nextInt();
+
+            ADJ.get(from).add(to);
+            ADJ.get(to).add(from);
+        }
+    }
+
+    private static void pro() {
+        int root = 1;
+        rec(root, -1);
+
+        int result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result += DP[root][i];
+            result %= MOD;
+        }
+
+        sb.append(result);
+    }
+
+    private static void rec(int node, int parent) {
+        List<Integer> childs = ADJ.get(node);
+        if (childs.size() == 1 && childs.get(0) == parent) {
+            return;
+        }
+
+        for (int child : childs) {
+            if (child == parent) continue;
+
+            rec(child, node);
+
+            acc(node, child);
+        }
+    }
+
+    // 자신을 선택하지 않는 경우와 자신을 선택하는 경우
+    private static void acc(int node, int child) {
+        int point = POINT[node];
+        for (int i = 0; i <= 9; i++) {
+            DP[node][i] += DP[child][i];
+            DP[node][i] %= MOD;
+
+            if (point <= i) {
+                DP[node][point] += DP[child][i];
+                DP[node][point] %= MOD;
+            }
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
     }
     
 }
