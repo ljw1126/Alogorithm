cdd3dde50823db0c3b17a8436ee1ee27f441faaf
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-03 21:31:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,104 +2,108 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-    static int N, K;
-    static int[] dist;
-    static boolean[] visit;
-
-    static void input() {
-        N = scan.nextInt();
-        K = scan.nextInt();
-        visit = new boolean[100005];
-        dist = new int[100005];
-    }
-
-    static void bfs() {
-        Queue<Integer> Q = new LinkedList<>();
-        Q.add(N);
-        visit[N] = true;
-        dist[N] = 0;
- 
-        while(!Q.isEmpty()){
-            int x = Q.poll();
-            int nx1 = x +1, nx2 = x - 1, nx3 = x * 2;
-          
-            if(0 <= nx1 && nx1 <= 100000 && !visit[nx1]){
-                visit[nx1] = true;
-                dist[nx1] = dist[x]+1;
-                Q.add(nx1);
-            }
-
-            if(0 <= nx2 && nx2 <= 100000 && !visit[nx2]){
-                visit[nx2] = true;
-                dist[nx2] = dist[x]+1;
-                Q.add(nx2);
-            }
-
-            if(0 <= nx3 && nx3 <= 100000 && !visit[nx3]){
-                visit[nx3] = true;
-                dist[nx3] = dist[x]+1;
-                Q.add(nx3);
-            }
-        }
-    }
-
-    static void pro() {
-        bfs();
-        System.out.println(dist[K]);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
     }
 
+    private static int N, K;
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+    private static void input() {
+        N = inputProcessor.nextInt();
+        K = inputProcessor.nextInt();
+    }
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
+    private static void pro() {
+        bfs();
+    }
+
+    private static void bfs() {
+        Deque<Integer> que = new ArrayDeque<>();
+        que.add(N);
+
+        int[] dist = new int[100001];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[N] = 0;
+
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+
+            if (cur == K) break;
+
+            int next = cur - 1;
+            if (0 <= next && next <= 100000 && dist[next] > dist[cur] + 1) {
+                dist[next] = dist[cur] + 1;
+                que.add(next);
+            }
+
+            next = cur + 1;
+            if (0 <= next && next <= 100000 && dist[next] > dist[cur] + 1) {
+                dist[next] = dist[cur] + 1;
+                que.add(next);
+            }
+
+            next = cur * 2;
+            if (0 <= next && next <= 100000 && dist[next] > dist[cur] + 1) {
+                dist[next] = dist[cur] + 1;
+                que.add(next);
+            }
         }
 
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
+        sb.append(dist[K]);
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next() {
+        public String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
             return st.nextToken();
         }
 
-        int nextInt() {
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
             return Integer.parseInt(next());
         }
 
-        long nextLong() {
+        public long nextLong() {
             return Long.parseLong(next());
         }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
+    
 }
