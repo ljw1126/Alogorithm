6782c2369807ff1a370186af8384f82e33a7f431
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-04 11:15:18 +0900

    [Silver I] Title: 나무 탈출, Time: 1944 ms, Memory: 205260 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -3,103 +3,52 @@ import java.io.*;
 
 public class Main {
     
-    static FastReader scan = new FastReader();
-   
-    static int n,ans;
-    static ArrayList<Integer>[] adj;
-    static boolean[] visit;
+   static int N, ANS;
+    static List<Integer>[] tree;
 
-    static void input(){
-        n = scan.nextInt();
-        adj = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        tree = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) tree[i] = new ArrayList();
+
+        for(int i = 1; i <= (N - 1); i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            tree[from].add(to);
+            tree[to].add(from);
         }
-        for (int i = 1; i < n; i++) {
-            int x = scan.nextInt(), y = scan.nextInt();
-            adj[x].add(y);
-            adj[y].add(x);
-        }
-   
-        visit = new boolean[n+1];
+
+        br.close();
     }
 
-    static void dfs(int node, int cnt){
-
-        visit[node] = true;
-        
-        for(int child : adj[node]){
-            if(visit[child]) continue;
-            dfs(child,cnt+1);
+    static void dfs(int node, int parent, int depth) {
+        if(node != 1 && tree[node].size() == 1 && tree[node].get(0) == parent) {
+            ANS += depth;
+            return;
         }
 
-        if(node != 1 && adj[node].size() == 1){
-            ans += cnt;
-        }
+        for(int child : tree[node]) {
+            if(child == parent) continue;
 
+            dfs(child, node,depth + 1);
+        }
     }
 
     static void pro() {
-        dfs(1,0);
+        dfs(1, -1,  0);
 
-        if(ans %2 == 0){
-            System.out.println("No");
-        }else{
-            System.out.println("Yes");
-        }
-    
+        System.out.println(ANS % 2 == 1 ? "Yes" : "No");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         input();
         pro();
     }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
+    
 }
