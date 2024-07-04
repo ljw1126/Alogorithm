f4ffd6e4f2ecb565c63cf31eaf8daf1c96734303
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-04 17:59:40 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 356 ms, Memory: 26708 KB, Score: 66 point -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -7,24 +7,6 @@ public class Main {
     static long K;
     static int[] foods;
 
-    static class Info {
-        private int start;
-        private long point;
-
-        public Info(int start, long point) {
-            this.start = start;
-            this.point = point;
-        }
-
-        public int start() {
-            return this.start;
-        }
-
-        public long point() {
-            return this.point;
-        }
-    }
-
     static void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -39,41 +21,37 @@ public class Main {
         }
     }
 
-    static void pro() throws Exception {
-        List<Info>[] infos = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) infos[i] = new ArrayList<>();
+    static void twoPointer() {
 
-        long acc = 0;
-        for(int L = 1, R = 0; L <= N; L++) { // 초기화 실수 가능
-            while(acc < K && R + 1 <= N) {
-                R += 1;
-                acc += foods[R];
+        long[] _dp = new long[N + 1];
+
+        int R = 1;
+        long maxleft = 0;
+        int sum = 0;
+        for(int L = 1; L <= N; L++) {
+
+            maxleft = Math.max(maxleft, _dp[L - 1]);
+
+            while(sum < K && R <= N) {
+                sum += foods[R++];
             }
 
-            if(K <= acc) { // 기준치 만족도 이상일 경우
-                infos[R].add(new Info(L, acc - K));
+            if(sum >= K) {
+                _dp[R - 1] = Math.max(_dp[R - 1], maxleft + (sum - K));
             }
 
-            acc -= foods[L];
+            sum -= foods[L];
         }
 
-        long[] dp = new long[N + 1];
-        for(int R = 1; R <= N; R++) { // isEmpty일대 continue 빼니 다 맞음
-            dp[R] = dp[R - 1]; // 이걸 빼먹음..
-            for(Info info : infos[R]) {
-                dp[R] = Math.max(dp[R], dp[info.start() - 1] + info.point());
-            }
-        }
+        long ans = 0;
+        for(int i = 1; i<= N; i++) ans = Math.max(ans, _dp[i]);
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(dp[N] + "");
-        bw.flush();
-        bw.close();
+        System.out.println(ans);
     }
 
     public static void main(String[] args) throws Exception {
         input();
-        pro();
+        twoPointer();
     }
     
 }
