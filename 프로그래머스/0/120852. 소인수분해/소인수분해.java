208c8f3bfcb2b9220145375a1f746e77c30458c5
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-08 15:58:46 +0900

    [level 0] Title: 소인수분해, Time: 2.50 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -3,32 +3,17 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int n) {
-        boolean[] isPrime = new boolean[10001];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-        
-        for(int i = 2; i <= 10000; i++) {
-            for(int j = i; j <= 10000; j += i) {
-                if(i == j) continue;
-                
-                isPrime[j] = false;
-            }
-        }
-        
         int operand = 2;
-        Set<Integer> resultSet = new HashSet();
+        Set<Integer> resultSet = new LinkedHashSet();
         while(n > 0 && operand <= 10000) {
-            if(isPrime[operand]) {
-                while(n % operand == 0) {
-                    resultSet.add(operand);
-                    n /= operand;
-                }
+            while(n % operand == 0) {
+                resultSet.add(operand);
+                n /= operand;
             }
-            
+
             operand += 1;
         }
         
-        return resultSet.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return resultSet.stream().mapToInt(Integer::intValue).toArray();
     }
 }
