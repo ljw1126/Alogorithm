dbc25b2ee05d5c2562471b8d8d687e3429f94cb4
Lee jinwoo <leejinwoo1126@gmail.com>
2024-07-13 20:02:50 +0900

    [level 0] Title: 세 개의 구분자, Time: 53.47 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myStr) {
+        myStr = myStr.replaceAll("[abc]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = "".equals(myStr) ? "EMPTY" : myStr;
+        
+        return myStr.split(",");
+    }
+    
+    public String[] solve(String myStr) {
         String[] tokens = myStr.split("[abc]");
         
         if(tokens.length == 0) return new String[] {"EMPTY"};
