7ad5f2e4b59cbce3bb6d355c2a531039f956c3a2
Lee jinwoo <leejinwoo1126@gmail.com>
2024-12-16 14:23:26 +0900

    [Silver III] Title: N과 M (5), Time: 312 ms, Memory: 28676 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -2,89 +2,100 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] nums, selected,used; 
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-    static StringBuilder sb = new StringBuilder();
+    private static int N, M;
+    private static int[] DATA;
 
-    static void input(){
-        FastReader scan = new FastReader();
-        N = scan.nextInt();
-        M = scan.nextInt();
-
-        nums = new int[N+1]; // 주어진 숫자 담겨지는 배열 
-        selected = new int[M+1];// 결과값 담기는 배열
-        used = new int[N+1];
-
-        for(int i=1; i <= N ; i++) nums[i] = scan.nextInt();
-
-        Arrays.sort(nums, 1, N+1);
-    }
-
-    static void rec_func(int k){
-        if(k == M+1){
-           for(int i = 1 ; i <= M ; i++) sb.append(selected[i]).append(' ');
-           sb.append('\n');
-        }else{  
-            for(int cand = 1; cand <= N ; cand++){
-                if(used[cand] == 1 ) continue;
-                selected[k] = nums[cand];   used[cand] = 1;
-                rec_func(k+1);
-                selected[k] = 0;            used[cand] = 0;
-            }           
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
-        rec_func(1);
-        System.out.println(sb.toString()); 
-
+        pro();
+        output();
     }
 
-    static class FastReader{
-        BufferedReader br;
-        StringTokenizer st; 
+    private static void input() {
+        N = inputProcessor.nextInt();
+        M = inputProcessor.nextInt();
 
-        public FastReader(){
-            br = new BufferedReader(new InputStreamReader(System.in));
+        DATA = new int[N];
+        for (int i = 0; i < N; i++) {
+            int v = inputProcessor.nextInt();
+            DATA[i] = v;
         }
 
-        public FastReader(String s) throws FileNotFoundException{
-            br = new BufferedReader(new FileReader(new File(s)));
+        Arrays.sort(DATA);
+    }
+
+    private static void pro() {
+        int[] selected = new int[M];
+        rec(0, selected, 0);
+    }
+
+    private static void rec(int count, int[] selected, int flag) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
         }
 
-        String next(){
-            while(st == null || !st.hasMoreElements()){
-                try{
+        for (int i = 0; i < N; i++) {
+            if ((flag & (1 << i)) != 0) continue;
+
+            selected[count] = DATA[i];
+            rec(count + 1, selected, flag | 1 << i);
+            selected[count] = -1;
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch(IOException e){
-                    e.printStackTrace();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
+
             return st.nextToken();
         }
 
-        int nextInt(){
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
             return Integer.parseInt(next());
         }
 
-        long nextLong(){
+        public long nextLong() {
             return Long.parseLong(next());
         }
-
-        double nextDouble(){
-            return Double.parseDouble(next());
-        }
-
-        String nextLine(){
-            String str = "";
-            try{
-                str = br.readLine();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-            return str;
-        }
     }
+    
 }
