b6f3ff2fecd0ed668875cda073c6d8a32ca0abb3
Lee jinwoo <leejinwoo1126@gmail.com>
2024-12-18 20:54:30 +0900

    [Gold IV] Title: 연구소, Time: 308 ms, Memory: 32128 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,135 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    
     private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         input();
         pro();
         output();
     }
 
     private static final int[][] DIR = {
-            {-1, 0},
             {1, 0},
             {0, 1},
+            {-1, 0},
             {0, -1}
     };
-    // 0 : 빈칸, 1 : 벽, 2 : 바이러스
-    private static int N, M, RESULT;
-    private static int[][] FACILITY;
-    private static List<Virus> virusList = new ArrayList<>();
-    private static List<Blank> blankList = new ArrayList<>();
-    private static int[] SELECTED;
+
+    private static int N, M;
+    private static int[][] FIELDS;
+    private static List<BLANK> BLANKS;
+    private static List<Virus> VIRUSES;
+    private static int RESULT;
 
     private static void input() {
         N = inputProcessor.nextInt();
         M = inputProcessor.nextInt();
+        RESULT = 0;
 
-        FACILITY = new int[N + 1][M + 1];
+        FIELDS = new int[N + 1][M + 1];
+        BLANKS = new ArrayList<>();
+        VIRUSES = new ArrayList<>();
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
-                int value = inputProcessor.nextInt();
-                FACILITY[i][j] = value;
+                int v = inputProcessor.nextInt();
+                FIELDS[i][j] = v;
 
-                if (value == 0) {
-                    blankList.add(new Blank(i, j));
-                }
-
-                if (value == 2) {
-                    virusList.add(new Virus(i, j));
+                if (v == 0) { // 빈칸
+                    BLANKS.add(new BLANK(i, j));
+                } else if (v == 2) { // 바이러스
+                    VIRUSES.add(new Virus(i, j));
                 }
             }
         }
-
-        SELECTED = new int[3]; // 벽은 3개만 세워야 한다
     }
 
-    // 64C3 벽을 3개 세워야한다
-    // 바이러스를 퍼뜨린다
-    private static void pro() {
-        createWall(0, 0);
-        sb.append(RESULT);
-    }
+    private static class BLANK {
+        private final int x;
+        private final int y;
 
-    private static void createWall(int start, int count) {
-        if (count == 3) {
-            calculate();
-            return;
-        }
-
-        for (int i = start; i < blankList.size(); i++) {
-            SELECTED[count] = i;
-            createWall(i + 1, count + 1);
-            SELECTED[count] = -1;
-        }
-    }
-
-    private static void calculate() {
-        int[][] temp = new int[N + 1][M + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                temp[i][j] = FACILITY[i][j];
-            }
-        }
-
-        // 벽을 세운다
-        for (int i = 0; i < 3; i++) {
-            Blank blank = blankList.get(SELECTED[i]);
-            temp[blank.x][blank.y] = 1;
-        }
-
-        // 바이러스를 퍼뜨린다
-        for (Virus virus : virusList) {
-            Deque<Integer> que = new ArrayDeque<>();
-            que.add(virus.x);
-            que.add(virus.y);
-
-            while (!que.isEmpty()) {
-                int x = que.poll();
-                int y = que.poll();
-
-                for (int i = 0; i < 4; i++) {
-                    int dx = x + DIR[i][0];
-                    int dy = y + DIR[i][1];
-
-                    if (dx < 1 || dy < 1 || dx > N || dy > M) continue;
-                    if (temp[dx][dy] != 0) continue;
-
-                    temp[dx][dy] = 2;
-                    que.add(dx);
-                    que.add(dy);
-                }
-            }
-        }
-
-        // 안전 영역을 센다
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (temp[i][j] == 0) {
-                    count += 1;
-                }
-            }
-        }
-
-        RESULT = Math.max(RESULT, count);
-    }
-
-    private static class Blank {
-        private int x;
-        private int y;
-
-        public Blank(int x, int y) {
+        public BLANK(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
     private static class Virus {
-        private int x;
-        private int y;
+        private final int x;
+        private final int y;
 
         public Virus(int x, int y) {
             this.x = x;
@@ -138,12 +67,72 @@ public class Main {
         }
     }
 
-    private static void output() throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+    private static void pro() {
+        createWall(0, 0);
+        sb.append(RESULT);
     }
+
+    private static void createWall(int count, int idx) {
+        if (count == 3) {
+            boolean[][] virus = spread();
+            int safety = 0;
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= M; j++) {
+                    if (FIELDS[i][j] == 0 && !virus[i][j]) {
+                        safety += 1;
+                    }
+                }
+            }
+
+            RESULT = Math.max(RESULT, safety);
+            return;
+        }
+
+        if (idx >= BLANKS.size()) return;
+
+        BLANK cur = BLANKS.get(idx);
+        FIELDS[cur.x][cur.y] = 1;
+        createWall(count + 1, idx + 1);
+
+        FIELDS[cur.x][cur.y] = 0;
+        createWall(count, idx + 1);
+    }
+
+    private static boolean[][] spread() {
+        boolean[][] visited = new boolean[N + 1][M + 1];
+        for (Virus v : VIRUSES) {
+            if (visited[v.x][v.y]) continue;
+            
+            dfs(v.x, v.y, visited);
+        }
+
+        return visited;
+    }
+
+    private static void dfs(int x, int y, boolean[][] visited) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int dx = DIR[i][0] + x;
+            int dy = DIR[i][1] + y;
+
+            if (dx < 1 || dy < 1 || dx > N || dy > M) continue;
+            if (visited[dx][dy]) continue;
+            if (FIELDS[dx][dy] != 0) continue;
+
+            dfs(dx, dy, visited);
+        }
+    }
+
+    private static void output() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     private static class InputProcessor {
         private BufferedReader br;
@@ -157,8 +146,8 @@ public class Main {
             while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
 
