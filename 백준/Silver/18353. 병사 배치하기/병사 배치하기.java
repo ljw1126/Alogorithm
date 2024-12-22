539944f95f69310e9a621923ee529ea695bac0f9
Lee jinwoo <leejinwoo1126@gmail.com>
2024-12-22 14:46:15 +0900

    [Silver II] Title: 병사 배치하기, Time: 140 ms, Memory: 14588 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,105 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    public static void main(String[] args) {
+        input();
+        pro();
+        output();
+    }
+
+    private static int N;
+    private static int[] POWER;
+
+    private static void input() {
+        N = inputProcessor.nextInt();
+        POWER = new int[N];
+        for (int i = 0; i < N; i++) {
+            POWER[i] = inputProcessor.nextInt();
+        }
+    }
+
+    private static void pro() {
+        int l = 0, r = N - 1;
+        while (l < r) {
+            int temp = POWER[l];
+            POWER[l] = POWER[r];
+            POWER[r] = temp;
+            l += 1;
+            r -= 1;
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (POWER[j] < POWER[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        
+        sb.append(N - max);
+    }
+
+    private static void output() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+    
+}
