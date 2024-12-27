1d9442183f977a4d9d6775ffa029cd694fe2940e
Lee jinwoo <leejinwoo1126@gmail.com>
2024-12-27 20:40:43 +0900

    [Gold IV] Title: 공유기 설치, Time: 352 ms, Memory: 40312 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -3,58 +3,113 @@ import java.io.*;
 
 public class Main {
     
-    static int N, C;
-    static int[] houses;
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+     private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 집의 개수
-        C = Integer.parseInt(st.nextToken()); // 공유기 개수
-
-        houses = new int[N + 1];
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            houses[i] = Integer.parseInt(st.nextToken());
-        }
-    }
-
-    // 그리드 기법 사용하여 정렬 후 1번 기준으로 조건 만족하는지 조회 : O(N)
-    static boolean isValidDistance(int D) {
-        int cnt = 1, last = houses[1];
-
-        for(int i = 2; i <= N; i++) {
-            if(houses[i] - last >= D) {
-                cnt += 1;
-                last = houses[i];
-            }
-        }
-
-        return cnt >= C;
-    }
-
-    static void pro() {
-        Arrays.sort(houses, 1, N + 1);
-
-        int result = 0;
-        int L = 0, R = 1000000000;
-        while(L <= R) {
-            int distance = (L + R) / 2;
-            if(isValidDistance(distance)) {
-                L = distance + 1;
-                result = distance;
-            } else {
-                R = distance - 1;
-            }
-        }
-
-        System.out.println(result);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         input();
         pro();
+        output();
     }
 
+    private static int N, C;
+    private static int[] DATA;
+
+    private static void input() {
+        N = inputProcessor.nextInt(); // 집의 개수
+        C = inputProcessor.nextInt();
+
+        DATA = new int[N + 1];
+        for(int i = 1; i <= N; i++) {
+            DATA[i] = inputProcessor.nextInt();
+        }
+    }
+
+    private static void pro() {
+        Arrays.sort(DATA, 1, N + 1);
+
+        int result = 0;
+        int L = 1;
+        int R = DATA[N] + 1;
+
+        while(L <= R) {
+            int d = (L + R) / 2;
+
+            if(isPossible(d, C)) {
+                result = d;
+                L = d + 1;
+            } else {
+                R = d - 1;
+            }
+        }
+
+        sb.append(result);
+    }
+
+    private static boolean isPossible(int d, int c) {
+        int count = 1;
+        int prev = DATA[1];
+        for(int i = 2; i <= N; i++) {
+            int cur = DATA[i];
+            int diff = cur - prev;
+
+            if(diff < d) continue;
+
+            count += 1;
+            prev = cur;
+        }
+        
+        return count >= c;
+    }
+
+    private static void output() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+    
 }
