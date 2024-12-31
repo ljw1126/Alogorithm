f2a07f3c72f9dcf3be78087c49eb2cf7a661fb88
Lee jinwoo <leejinwoo1126@gmail.com>
2024-12-31 17:24:28 +0900

    [level 4] Title: 가사 검색, Time: 611.36 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.java
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.java
@@ -3,98 +3,74 @@ import java.io.*;
 
 class Solution {
     private static StringBuilder sb = new StringBuilder();
-    private List<List<String>> forward = new ArrayList<>();
-    private List<List<String>> reverse = new ArrayList<>();
+    private List<List<String>> forward;
+    private List<List<String>> reverse;
     
     public int[] solution(String[] words, String[] queries) {
-        for(int i = 0; i <= 100000; i++) {
+        forward = new ArrayList<>();
+        reverse = new ArrayList<>();
+        for(int i = 0; i <= 10_000; i++) {
             forward.add(new ArrayList<>());
             reverse.add(new ArrayList<>());
         }
-        
+
         for(String w : words) {
             int len = w.length();
             forward.get(len).add(w);
-            reverse.get(len).add(reversed(w));
+            reverse.get(len).add(reverseFrom(w));
         }
-        
-        for(int i = 0; i <= 100000; i++) {
-            if(forward.get(i).isEmpty()) continue;
-            
+
+        for(int i = 1; i <= 10_000; i++) {
+            if(forward.get(i).isEmpty() || reverse.get(i).isEmpty()) continue;
+
             Collections.sort(forward.get(i));
             Collections.sort(reverse.get(i));
         }
-        
+
         int[] answer = new int[queries.length];
-        int idx = 0;
-        for(String q : queries) {
-            if(q.startsWith("?")) {
-                answer[idx++] = countByRange(reverse, reversed(q));
+        for(int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            int len = query.length();
+            if(query.startsWith("?")) {
+                answer[i] = countByRange(reverse.get(len), reverseFrom(query));
             } else {
-                answer[idx++] = countByRange(forward, q);
+                answer[i] = countByRange(forward.get(len), query);
             }
         }
         
         return answer;
     }
     
-    private String reversed(String s) {
-        String result = sb.append(s).reverse().toString();
+    private static String reverseFrom(String word) {
+        String result = sb.append(word).reverse().toString();
         sb.setLength(0);
         return result;
     }
-   
-    private int countByRange(List<List<String>> words, String query) {
-        int left = lowerBound(words, query);
-        int right = upperBound(words, query);
-        
+
+    private static int countByRange(List<String> words, String query) {
+        if(words.isEmpty()) return 0;
+
+        int left = binarySearch(words, query.replaceAll("\\?", "a"));
+        int right = binarySearch(words, query.replaceAll("\\?", "z"));
+
         return right - left;
     }
-    
-    private int lowerBound(List<List<String>> words, String query) {
-        int len = query.length();
-        List<String> arr = words.get(len);
-        if(arr.isEmpty()) return -1;
-        
-        query = query.replaceAll("\\?", "a");
+
+    // 음수가 되는 가장 첫번째
+    private static int binarySearch(List<String> words, String query) {
         int L = 0;
-        int R = arr.size();
-        
+        int R = words.size();
+
         while(L < R) {
             int mid = (L + R) / 2;
-            
-            int v = query.compareTo(arr.get(mid));
-            
-            if(v < 0) {
+
+            if(query.compareTo(words.get(mid)) < 0) {
                 R = mid;
             } else {
                 L = mid + 1;
             }
         }
-        
-        return R;
-    }
-    
-    private int upperBound(List<List<String>> words, String query) {
-        int len = query.length();
-        List<String> arr = words.get(len);
-        if(arr.isEmpty()) return -1;
-        
-        query = query.replaceAll("\\?", "z");
-        int L = 0;
-        int R = arr.size();
-        
-        while(L < R) {
-            int mid = (L + R) / 2;
-            int v = query.compareTo(arr.get(mid));
-            
-            if(v < 0) {
-                R = mid;
-            } else {
-                L = mid + 1;
-            }
-        }
-            
+
         return R;
     }
 }
