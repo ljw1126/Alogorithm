94b69a650064de970f9793b29305f30655ecf67c
Lee jinwoo <leejinwoo1126@gmail.com>
2025-01-02 23:26:54 +0900

    [level 2] Title: 문자열 압축, Time: 83.38 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,42 +2,55 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
+    private static StringBuilder sb = new StringBuilder();
+    private String S;
     public int solution(String s) {
+        if(s.length() == 1) return 1;
+        
         int answer = 1001;
-        
-        
-        int half = s.length() / 2 + 1;
-        StringBuilder str = new StringBuilder();
+        S = s;
+
+        int half = s.length() / 2;
         for(int len = 1; len <= half; len++) {
+            String compressed = compress(len, sb);
+            
+            answer = Math.min(answer, compressed.length());
 
-            int idx = 0;
-            while(idx < (s.length() - len)) {
-                String target = s.substring(idx, idx + len);
-                int repeated = 1 + rec(s, target, idx + len, len);
-
-                String compress = repeated == 1 ? target : (repeated + target);
-                str.append(compress);
-
-                idx += (repeated == 1 ? len : repeated * len);
-            }
-
-            if(idx < s.length()) {
-                str.append(s.substring(idx));
-            }
-
-            answer = Math.min(answer, str.length());
-            str.setLength(0);
+            sb.setLength(0);
         }
         
         return answer;
     }
     
-    private static int rec(String base, String t, int start, int len) {
-        if(start + len > base.length()) return 0;
+    private String compress(int len, StringBuilder stringBuilder) {
+        int L = 0;
+        while(L <= S.length() - len) {
+            int R = L + len;
+            String base = S.substring(L, R);
+            int repeated = 1 + count(base, R, len);
 
-        String next = base.substring(start, start + len);
-        if(t.equals(next)) {
-            return 1 + rec(base, t, start + len, len);
+            if(repeated == 1) {
+                sb.append(base);
+            } else {
+                sb.append(repeated + base);
+            }
+
+            L += repeated * len;
+        }
+
+        if(L < S.length()) {
+            sb.append(S.substring(L));
+        }
+
+        return sb.toString();
+    }
+
+    private int count(String t, int from, int len) {
+        if(from >= S.length()) return 0;
+
+        String remain = S.substring(from);
+        if(remain.startsWith(t)) {
+            return 1 + count(t, from + len, len);
         }
 
         return 0;
