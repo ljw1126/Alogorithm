7fce3840ea4a31daf89f164d9c9bca68adcf6e83
Lee jinwoo <leejinwoo1126@gmail.com>
2025-01-02 20:46:36 +0900

    [level 3] Title: 기둥과 보 설치, Time: 2.92 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,132 +1,117 @@
 class Solution {
     public int[][] solution(int n, int[][] build_frame) {
-        Result base = new Result(n);
+        Board board = new Board(n);
 
-        for(int[] frame : build_frame) {
-            int x = frame[0];
-            int y = frame[1];
-            int a = frame[2]; // 0: 기둥, 1: 보
-            int b = frame[3]; // 0: 삭제, 1: 설치
+        for (int[] bf : build_frame) {
+            int x = bf[0];
+            int y = bf[1];
+            int a = bf[2]; // 0 : 기둥, 1 : 보
+            int b = bf[3]; // 0 : 삭제, 1 : 설치 
 
-            base.process(x, y, a, b);
+            board.execute(x, y, a, b);
         }
 
-        return base.getResult();
+        int[][] answer = new int[board.count][3];
+        int idx = 0;
+        boolean[][] pole = board.pole;
+        boolean[][] bo = board.bo;
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(pole[i][j]) {
+                    answer[idx++] = new int[]{i, j, 0};
+                }
+
+                if(bo[i][j]) {
+                    answer[idx++] = new int[]{i, j, 1};
+                }
+            }
+        }
+        return answer;
     }
     
-     private static class Result {
-        private final boolean[][] pole;
-        private final boolean[][] bar;
+     private static class Board {
         private final int n;
-        private int count = 0;
+        private final boolean[][] pole;
+        private final boolean[][] bo;
+        private int count;
 
-        public Result(int n) {
-            this(new boolean[n + 2][n + 2], new boolean[n + 2][n + 2], n);
-        }
-
-        public Result(boolean[][] pole, boolean[][] bar, int n) {
-            this.pole = pole;
-            this.bar = bar;
+        public Board(int n) {
             this.n = n;
+            this.pole = new boolean[n + 2][n + 2];
+            this.bo = new boolean[n + 2][n + 2];
         }
 
-        public void process(int x, int y, int a, int b) {
-            if(a == 0 && b == 1) {
-                buildPole(x, y);
-            } else if(a == 0 && b == 0) {
-                deletePole(x, y);
-            } else if(a == 1 && b == 1) {
-                buildBar(x, y);
-            } else if(a == 1 && b == 0) {
-                deleteBar(x, y);
+        public void execute(int x, int y, int a, int b) {
+            if(a == 0) { // 기둥
+                if(b == 0) destroyPole(x, y);
+                else if(b == 1) buildPole(x, y);
+            } else if(a == 1){ // 보
+                if(b == 0) destroyBo(x, y);
+                else if(b == 1) buildBo(x, y);
             }
         }
 
-        //바닥이거나, 아래에 기둥이 있다면, 왼쪽에 보가 있다면, 오른쪽에 보가 있다면
         private void buildPole(int x, int y) {
-            if(checkPole(x, y)) {
+            if(buildablePole(x, y)) {
                 pole[x][y] = true;
                 count += 1;
             }
         }
 
-        private boolean checkPole(int x, int y) {
+        // 바닥 위에 있거나, 다른 기등 위에 있거나, 보의 한쪽 끝에 있거나
+        private boolean buildablePole(int x, int y) {
             return y == 0
-                    || (0 < y && pole[x][y - 1])
-                    || (0 < x && bar[x - 1][y])
-                    || bar[x][y];
+                  || (0 < y && pole[x][y - 1])
+                  || (0 < x && bo[x - 1][y])
+                  || (bo[x][y]);
         }
 
-        // 위에 기둥이 있는 경우, x == 0 (n)에 기둥이 있을대 , 양옆에 기둥없이 보가 있을대
-        private void deletePole(int x, int y) {
+        private void destroyPole(int x, int y) {
             pole[x][y] = false;
-            if(canDelete(x, y)) {
-                count -= 1;
-            } else {
+            if(invalid(x, y)) {
                 pole[x][y] = true;
+            } else {
+                count -= 1;
             }
         }
 
-        // 왼쪽 아래 기둥이 있거나, 오른쪽 아래에 기둥이 있다면, 양옆에 보가 있다면
-        private void buildBar(int x, int y) {
-            if(checkBar(x, y)) {
-                bar[x][y] = true;
+        private void buildBo(int x, int y) {
+            if(buildableBo(x, y)) {
+                bo[x][y] = true;
                 count += 1;
             }
         }
 
-        // 왼쪽, 오른쪽에 기둥이 있거나, 양옆에 보가 있는 경우
-        private boolean checkBar(int x, int y) {
+        // 한쪽 끝 부분이 기둥 위에 있거나, 또는 양쪽 끝부분이 다른 보와 동시에 연결되어 있는 경우
+        private boolean buildableBo(int x, int y) {
             return (0 < y && pole[x][y - 1])
                     || (0 < y && pole[x + 1][y - 1])
-                    || (0 < x && bar[x - 1][y] && bar[x + 1][y]);
+                    || ((0 < x && bo[x - 1][y]) && bo[x + 1][y]);
         }
 
-        private void deleteBar(int x, int y) {
-            bar[x][y] = false;
-            if(canDelete(x, y)) {
-                count -= 1;
+        private void destroyBo(int x, int y) {
+            bo[x][y] = false;
+            if(invalid(x, y)) {
+                bo[x][y] = true;
             } else {
-                bar[x][y] = true;
+                count -= 1;
             }
         }
 
-        private boolean canDelete(int x, int y) {
+        private boolean invalid(int x, int y) {
             int minX = Math.max(0, x - 1);
-            int maxX = Math.min(n, x + 1);
+            int maxX = Math.min(x + 1, n);
             int minY = Math.max(0, y - 1);
-            int maxY = Math.min(n, y + 1);
-            
+            int maxY = Math.min(y + 1, n);
             for(int i = minX; i <= maxX; i++) {
                 for(int j = minY; j <= maxY; j++) {
-                    if(pole[i][j] && !checkPole(i, j)) {
-                        return false;
-                    }
+                    if(pole[i][j] && !buildablePole(i, j)) return true;
 
-                    if(bar[i][j] && !checkBar(i, j)) {
-                        return false;
-                    }
+                    if(bo[i][j] && !buildableBo(i, j)) return true;
                 }
             }
 
-            return true;
-        }
-
-        public int[][] getResult() {
-            int[][] result = new int[count][3];
-            int idx = 0;
-            for(int i = 0; i <= n; i++) {
-                for(int j = 0; j <= n; j++) {
-                    if(pole[i][j]) {
-                        result[idx++] = new int[] {i, j, 0};
-                    }
-
-                    if(bar[i][j]) {
-                        result[idx++] = new int[] {i, j, 1};
-                    }
-                }
-            }
-            return result;
+            return false;
         }
     }
 }
