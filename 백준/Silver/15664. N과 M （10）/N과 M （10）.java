01f67ac28064c5a5a1458eb4dad03f352ed6de2e
Lee jinwoo <leejinwoo1126@gmail.com>
2025-05-12 16:21:39 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    private static int N, M;
+    private static int[] DATA;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
+    }
+
+    private static void input() {
+        N = inputProcessor.nextInt();
+        M = inputProcessor.nextInt();
+
+        DATA = new int[N];
+        for (int i = 0; i < N; i++) {
+            int v = inputProcessor.nextInt();
+            DATA[i] = v;
+        }
+
+        Arrays.sort(DATA);
+    }
+
+    private static void pro() {
+        int[] selected = new int[M];
+        rec(0, 0, selected);
+    }
+
+    private static void rec(int start, int count, int[] selected) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = start; i < N; i++) {
+            if (prev == DATA[i]) continue;
+
+            prev = DATA[i];
+            selected[count] = DATA[i];
+
+            rec(i + 1, count + 1, selected);
+
+            selected[count] = -1;
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+    
+}
