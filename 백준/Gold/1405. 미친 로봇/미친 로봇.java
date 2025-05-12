e81c5cfbd718537dad6b0fe80b6cd0f633354cd5
Lee jinwoo <leejinwoo1126@gmail.com>
2025-05-12 22:20:12 +0900

    [Gold IV] Title: 미친 로봇, Time: 152 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     
-    private static StringBuilder sb = new StringBuilder();
+     private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
     public static void main(String[] args) {
@@ -13,51 +13,51 @@ public class Main {
     }
 
     private static int N;
-    private static double[] percentage; // 동서남북
-    private static double result;
+    private static double[] percent;
 
     private static void input() {
         N = inputProcessor.nextInt();
-        percentage = new double[4];
+        percent = new double[4];
         for(int i = 0; i < 4; i++) {
-            int p = inputProcessor.nextInt();
-            percentage[i] = (double) p / 100;
+            int v = inputProcessor.nextInt();
+            if(v == 0) continue;
+
+            percent[i] = v * 0.01;
         }
     }
 
-    private static void pro() {
-        boolean[][] maps = new boolean[31][31];
-        maps[15][15] = true;
-
-        rec(15, 15, 0, 1.0, maps);
-
-        sb.append(result);
-    }
-
-    private static int[][] dir = {
+    // 0 : 동, 1 : 서, 2: 남, 3: 북
+    private static final int[][] dir = {
             {0, 1},
             {0, -1},
             {1, 0},
             {-1, 0}
     };
 
-    private static void rec(int x, int y, int cnt, double value, boolean[][] maps) {
-        if(cnt == N) {
-            result += value;
-            return;
+    private static void pro() {
+        boolean[][] visited = new boolean[30][30];
+        visited[15][15] = true;
+        sb.append(backTracking(15, 15, visited, 0, 1.0));
+    }
+
+    private static double backTracking(int x, int y, boolean[][] visited, int count, double weight) {
+        if(count == N) {
+            return weight;
         }
 
+        double result = 0.0;
         for(int i = 0; i < 4; i++) {
             int dx = x + dir[i][0];
             int dy = y + dir[i][1];
 
-            if(dx < 1 || dy < 1 || dx >= 30 || dy >= 30) continue;
-            if(maps[dx][dy]) continue;
+            if(visited[dx][dy]) continue;
 
-            maps[dx][dy] = true;
-            rec(dx, dy, cnt + 1, value * percentage[i], maps);
-            maps[dx][dy] = false;
+            visited[dx][dy] = true;
+            result += backTracking(dx, dy, visited, count + 1, weight * percent[i]);
+            visited[dx][dy] = false;
         }
+
+        return result;
     }
 
     private static void output() {
