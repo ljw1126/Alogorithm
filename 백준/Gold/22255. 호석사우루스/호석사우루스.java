88b534481b97b9a47afb326e31bce9aa2a2ef832
Lee jinwoo <leejinwoo1126@gmail.com>
2025-05-15 15:29:22 +0900

    [Gold II] Title: 호석사우루스, Time: 188 ms, Memory: 17172 KB -BaekjoonHub

--- 백준/Gold/22255. 호석사우루스/호석사우루스.java
+++ 백준/Gold/22255. 호석사우루스/호석사우루스.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-   private static StringBuilder sb = new StringBuilder();
+    
+    private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
     public static void main(String[] args) {
@@ -11,20 +12,13 @@ public class Main {
         output();
     }
 
-    private static int[][] DIR = {
-            {-1, 0},
-            {1, 0},
-            {0, 1},
-            {0, -1}
-    };
     private static int n, m, sx, sy, ex, ey;
     private static int[][] board;
 
-
     private static void input() {
-        n = inputProcessor.nextInt(); // 행
-        m = inputProcessor.nextInt(); // 열
-        // 출발지, 도착지
+        n = inputProcessor.nextInt();
+        m = inputProcessor.nextInt();
+
         sx = inputProcessor.nextInt();
         sy = inputProcessor.nextInt();
         ex = inputProcessor.nextInt();
@@ -39,58 +33,73 @@ public class Main {
     }
 
     private static void pro() {
-        boolean[][][] visited = new boolean[n + 1][m + 1][3];
+        sb.append(dijkstra());
+    }
 
+    private static final int[][] dir = {
+            {-1, 0},
+            {1, 0},
+            {0, 1},
+            {0, -1},
+    };
+
+    private static int dijkstra() {
         Queue<Dinor> pq = new PriorityQueue<>();
-        pq.add(new Dinor(sx, sy, 0, 1));
-        int result = -1;
+        pq.add(new Dinor(sx, sy, 1, 0));
 
-        while(!pq.isEmpty()) {
-            Dinor cur = pq.poll();
-
-            if(cur.x == ex && cur.y == ey) {
-                result = cur.dist;
-                break;
-            }
-
-            int start = 0;
-            int end = 4;
-            if(cur.turn == 1) {
-                end = 2;
-            }
-
-            if(cur.turn == 2) {
-                start = 2;
-            }
-
-            for(int i = start; i < end; i++) {
-                int dx = cur.x + DIR[i][0];
-                int dy = cur.y + DIR[i][1];
-
-                if(dx < 1 || dx > n || dy < 1 || dy > m) continue;
-                if(board[dx][dy] == -1) continue;
-                if(visited[dx][dy][cur.turn]) continue;
-
-                visited[dx][dy][cur.turn] = true;
-                pq.add(new Dinor(dx, dy, cur.dist + board[dx][dy], (cur.turn + 1) % 3));
+        int[][][] dist = new int[n + 1][m + 1][4];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
             }
         }
 
+        int result = Integer.MAX_VALUE;
+        while(!pq.isEmpty()) {
+            Dinor cur = pq.poll();
 
-        sb.append(result);
+            if(dist[cur.x][cur.y][cur.turn] < cur.dist) continue;
+            if(cur.x == ex && cur.y == ey) {
+                result = Math.min(result, cur.dist);
+                continue;
+            }
+
+            int start = 0;
+            int end = 3;
+            if(cur.turn == 1) end = 1;
+            else if(cur.turn == 2) start = 2;
+
+            for(int i = start; i <= end; i++) {
+                int dx = cur.x + dir[i][0];
+                int dy = cur.y + dir[i][1];
+
+                if(dx < 1 || dy < 1 || dx > n || dy > m) continue;
+                if(board[dx][dy] == -1) continue;
+
+                int turn = (cur.turn + 1) % 3;
+                if(dist[dx][dy][turn] > cur.dist + board[dx][dy]) {
+                    dist[dx][dy][turn] = cur.dist + board[dx][dy];
+                    Dinor next = new Dinor(dx, dy, (cur.turn + 1) % 3, dist[dx][dy][turn]);
+                    pq.add(next);
+                }
+            }
+
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 
     private static class Dinor implements Comparable<Dinor>{
-        private final int x;
-        private final int y;
-        private final int dist;
-        private final int turn;
+        private int x;
+        private int y;
+        private int turn;
+        private int dist;
 
-        public Dinor(int x, int y, int dist, int turn) {
+        public Dinor(int x, int y, int turn, int dist) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
             this.turn = turn;
+            this.dist = dist;
         }
 
         public int compareTo(Dinor o) {
@@ -147,5 +156,4 @@ public class Main {
             return Long.parseLong(next());
         }
     }
-    
 }
