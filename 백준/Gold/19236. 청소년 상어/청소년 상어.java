be87a8f21d14daaeba15c2c98da549f7a686076a
Lee jinwoo <leejinwoo1126@gmail.com>
2025-05-17 12:41:53 +0900

    [Gold I] Title: 청소년 상어, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
     private static StringBuilder sb = new StringBuilder();
     private static InputProcessor inputProcessor = new InputProcessor();
 
@@ -12,7 +11,7 @@ public class Main {
         output();
     }
 
-    private static final int[][] DIR = {
+    private static final int[][] dir = {
             {-1, 0},
             {-1, -1},
             {0, -1},
@@ -20,101 +19,107 @@ public class Main {
             {1, 0},
             {1, 1},
             {0, 1},
-            {-1, 1},
+            {-1, 1}
     };
 
-    private static int[][][] maps;
-    private static int result;
+    private static int[][][] sea;
 
     private static void input() {
-        maps = new int[5][5][2]; // 0 : 번호, 1 : 방향
-        for(int i = 1; i <= 4; i++) {
-            for(int j = 1; j <= 4; j++) {
-                int no = inputProcessor.nextInt();
-                int dir = inputProcessor.nextInt();
+        sea = new int[4][4][2];
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++) {
+                int a = inputProcessor.nextInt(); // 물고기 번호
+                int b = inputProcessor.nextInt(); // 방향
 
-                maps[i][j][0] = no;
-                maps[i][j][1] = dir - 1;
+                sea[i][j][0] = a;
+                sea[i][j][1] = b - 1;
             }
         }
     }
 
     private static void pro() {
-        rec(maps, 1, 1, 0);
-
-        sb.append(result);
+        int fishNo = sea[0][0][0];
+        sea[0][0][0] = -1;
+        sb.append(backTracking(0, 0, fishNo , sea));
     }
 
-    private static void rec(int[][][] arr, int sharkX, int sharkY, int value) {
-        int[][][] _arr = copy(arr);
+    // 현재 상어의 위치와 방향이 중요하지 번호는 의미가 없음
+    private static int backTracking(int x, int y, int sum, int[][][] field) {
+        int[][][] copied = copy(field);
 
-        int total = value + _arr[sharkX][sharkY][0];
-        _arr[sharkX][sharkY][0] = -1;
+        int curDir = copied[x][y][1];
 
-        moveAllFishes(_arr, sharkX, sharkY);
+        moveFish(copied, x, y);
 
-        List<int[]> nexts = findNextPositions(_arr, sharkX, sharkY);
-        if(nexts.size() == 0) {
-            result = Math.max(result, total);
-            return;
+        List<int[]> targets = searchNextTarget(x, y, curDir, copied);
+
+        if(targets.isEmpty()) {
+            return sum;
         }
 
-        _arr[sharkX][sharkY][0] = -1;
-        _arr[sharkX][sharkY][1] = -1;
-        for(int[] next : nexts) {
-            rec(_arr, next[0], next[1], total);
+        copied[x][y][1] = -1;
+        int result = 0;
+        for(int[] next : targets) {
+            int fishNo = copied[next[0]][next[1]][0];
+
+            copied[next[0]][next[1]][0] = -1;
+
+            result = Math.max(result, backTracking(next[0], next[1], sum + fishNo, copied));
+
+            copied[next[0]][next[1]][0] = fishNo;
         }
+
+        return result;
     }
 
-    private static int[][][] copy(int[][][] base) {
-        int[][][] temp = new int[5][5][2];
-        for(int i = 1; i <= 4; i++) {
-            for(int j = 1; j <= 4; j++) {
-                temp[i][j][0] = base[i][j][0];
-                temp[i][j][1] = base[i][j][1];
+    private static int[][][] copy(int[][][] field) {
+        int[][][] result = new int[4][4][2];
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                result[i][j][0] = field[i][j][0];
+                result[i][j][1] = field[i][j][1];
             }
         }
 
-        return temp;
+        return result;
     }
 
-    private static void moveAllFishes(int[][][] arr, int sharkX, int sharkY) {
-        for(int i = 1; i <= 16; i++) {
-            int[] position = findFish(arr, i);
+    private static int[][][] moveFish(int[][][] field, int sharkX, int sharkY) {
+        for(int k = 1; k <= 16; k++) {
+            int[] fish = findFish(k, field);
 
-            if(position == null) continue;
+            if(fish == null) continue;
 
-            int x = position[0];
-            int y = position[1];
-            int no = arr[x][y][0];
-            int dir = arr[x][y][1];
+            int x = fish[0];
+            int y = fish[1];
+            int fishNo = field[x][y][0];
+            int fishDir = field[x][y][1];
+            for(int i = 0; i < 8; i++) {
+                int nextDir = (fishDir + i) % 8;
+                int dx = x + dir[nextDir][0];
+                int dy = y + dir[nextDir][1];
 
-            for(int j = 0; j < 8; j++) {
-                int _dir = (dir + j) % 8;
-                int dx = x + DIR[_dir][0];
-                int dy = y + DIR[_dir][1];
+                if(dx < 0 || dy < 0 || dx >= 4 || dy >= 4) continue;
+                if(sharkX == dx && sharkY == dy) continue;
 
-                if(dx < 1 || dy < 1 || dx > 4 || dy > 4) continue;
-                if(dx == sharkX && dy == sharkY) continue;
+                // 빈칸이나 다른 물고기
+                field[x][y][0] = field[dx][dy][0];
+                field[x][y][1] = field[dx][dy][1];
 
-                int _no = arr[dx][dy][0];
-                int __dir = arr[dx][dy][1];
-
-                arr[dx][dy][0] = no;
-                arr[dx][dy][1] = _dir;
-
-                arr[x][y][0] = _no;
-                arr[x][y][1] = __dir;
+                field[dx][dy][0] = fishNo;
+                field[dx][dy][1] = nextDir;
 
                 break;
             }
         }
+
+        return field;
     }
 
-    private static int[] findFish(int[][][] arr, int no) {
-        for(int i = 1; i <= 4; i++) {
-            for(int j = 1; j <= 4; j++) {
-                if(arr[i][j][0] == no) {
+    private static int[] findFish(int no, int[][][] field) {
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                if(field[i][j][0] == no) {
                     return new int[] {i, j};
                 }
             }
@@ -123,21 +128,22 @@ public class Main {
         return null;
     }
 
-    private static List<int[]> findNextPositions(int[][][] arr, int sharkX, int sharkY) {
-        List<int[]> result = new ArrayList<>();
-        int dir = arr[sharkX][sharkY][1];
-        int x = sharkX;
-        int y = sharkY;
-        for(int i = 0; i < 4; i++) {
-            x += DIR[dir][0];
-            y += DIR[dir][1];
+    private static List<int[]> searchNextTarget(int x, int y, int direction, int[][][] moved) {
+        List<int[]> next = new ArrayList<>();
 
-            if(1 <= x && x <= 4 && 1 <= y && y <= 4 && arr[x][y][0] != -1) {
-                result.add(new int[] {x, y});
+        int dx = x;
+        int dy = y;
+        for (int i = 1; i <= 4; i++) {
+            dx += dir[direction][0];
+            dy += dir[direction][1];
+
+            if(dx < 0 || dy < 0 || dx >= 4 || dy >= 4) break;
+            if(moved[dx][dy][0] >= 1) {
+                next.add(new int[]{dx, dy});
             }
         }
 
-        return result;
+        return next;
     }
 
     private static void output() {
