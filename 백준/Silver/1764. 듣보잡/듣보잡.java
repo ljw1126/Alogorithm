beb82bd70bfe8202442c889dc7f89903217e7ba9
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-02 17:40:42 +0900

    [Silver IV] Title: 듣보잡, Time: 228 ms, Memory: 24132 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,26 +2,65 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static String[] A, B;
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    private static int N, M;
+    private static String[] A, B;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
+    }
 
     private static void input() {
-        InputProcessor inputProcessor = new InputProcessor();
         N = inputProcessor.nextInt();
         M = inputProcessor.nextInt();
 
         A = new String[N + 1];
         B = new String[M + 1];
 
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             A[i] = inputProcessor.nextLine();
         }
 
-        for(int i = 1; i <= M; i++) {
+        for (int i = 1; i <= M; i++) {
             B[i] = inputProcessor.nextLine();
         }
     }
+
+    private static void pro() {
+        List<String> result = new ArrayList<>();
+        Arrays.sort(B, 1, M + 1);
+
+        for (int i = 1; i <= N; i++) {
+            if (binarySearch(B, 1, M, A[i])) {
+                result.add(A[i]);
+            }
+        }
+
+        Collections.sort(result);
+        sb.append(result.size()).append("\n");
+        result.forEach(v -> sb.append(v).append("\n"));
+    }
+
+    private static boolean binarySearch(String[] arr, int start, int end, String target) {
+        int L = start;
+        int R = end;
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            int result = arr[mid].compareTo(target);
+
+            if (result == 0) return true;
+            else if (result < 0) L = mid + 1;
+            else R = mid - 1;
+        }
+
+        return false;
+    }
+
 
     private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -30,61 +69,31 @@ public class Main {
         bw.close();
     }
 
-    private static void pro() {
-        Arrays.sort(A, 1, N + 1); // 정렬 보장
-
-        List<String> result = new LinkedList<>();
-        for(int i = 1; i <= M; i++) {
-            if(binarySearch(A, B[i], 1, N)) {
-                result.add(B[i]);
-            }
-        }
-
-        Collections.sort(result);
-        sb.append(result.size()).append("\n");
-        for(String name : result) {
-            sb.append(name).append("\n");
-        }
-    }
-
-    private static boolean binarySearch(String[] arr, String target, int start, int end) {
-        int L = start;
-        int R = end;
-
-        while(L <= R) {
-            int mid = (L + R) / 2;
-
-            if(arr[mid].equals(target)) return true;
-            else if(arr[mid].compareTo(target) > 0) R = mid - 1;
-            else L = mid + 1;
-        }
-
-        return false;
-    }
-
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
-        output();
-    }
-
-    public static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
 
         public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         public String next() {
-            while(st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
             }
             return st.nextToken();
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         public int nextInt() {
@@ -93,18 +102,6 @@ public class Main {
 
         public long nextLong() {
             return Long.parseLong(next());
-        }
-
-        public String nextLine() {
-            String input = "";
-
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return input;
         }
     }
     
