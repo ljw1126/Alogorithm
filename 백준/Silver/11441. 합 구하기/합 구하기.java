bd8713c9f6d686988cd379bb7acac354996589e7
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-10 07:45:47 +0900

    [Silver III] Title: 합 구하기, Time: 488 ms, Memory: 50340 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.java
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -10,39 +10,30 @@ public class Main {
         output();
     }
 
-    private static int n, m;
-    private static int[] a;
-
-     private static void pro() throws IOException {
+    private static void pro() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        a = new int[n + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n + 1];
+        int[] acc = new int[n + 1];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             a[i] = Integer.parseInt(st.nextToken());
+            acc[i] = acc[i - 1] + a[i];
         }
 
-        m = Integer.parseInt(br.readLine());
-
+        int m = Integer.parseInt(br.readLine());
         for(int i = 1; i <= m; i++){
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(stringTokenizer.nextToken());
             int y = Integer.parseInt(stringTokenizer.nextToken());
+            int result = acc[y] - acc[x - 1];
 
-            sb.append(acc(x, y)).append("\n");
+            sb.append(result).append("\n");
         }
 
         br.close();
-    }
-
-    private static int acc(int from, int to) {
-        int result = 0;
-        for(int i = from; i <= to; i++){
-            result += a[i];
-        }
-
-        return result;
     }
 
     private static void output() throws IOException {
