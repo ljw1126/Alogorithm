6d0f6d372f8a3b2d4a1254083027345b54d6b17e
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-18 07:56:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.24 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,31 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] maps) {
-        
-        int row = maps.length;
-        int col = maps[0].length;
-        
-        int[][] dir = {
+            
+    private static final int[][] dir = {
             {1, 0},
             {-1, 0},
             {0, 1},
             {0, -1}
-        };
+    };
+    
+    public int solution(int[][] maps) {
+        int row = maps.length;
+        int col = maps[0].length;
+
+        int[][] dist = new int[row][col];
+        for(int i = 0; i < row; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        dist[0][0] = 1;
         
         Deque<Integer> que = new ArrayDeque<>();
         que.add(0);
         que.add(0);
         
-        int[][] dist = new int[row][col];
-        for(int i = 0; i < row; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        
-        dist[0][0] = 1;
         while(!que.isEmpty()) {
             int x = que.poll();
             int y = que.poll();
+            
+            if(x == row - 1 && y == col - 1) continue;
             
             for(int i = 0; i < 4; i++) {
                 int dx = x + dir[i][0];
@@ -35,16 +37,14 @@ class Solution {
                 if(maps[dx][dy] == 0) continue;
                 if(dist[dx][dy] != Integer.MAX_VALUE) continue;
                 
-                
                 dist[dx][dy] = dist[x][y] + 1;
                 que.add(dx);
                 que.add(dy);
             }
         }
         
-        int answer = dist[row - 1][col - 1];
-        if(answer == Integer.MAX_VALUE) return -1;
-        
-        return answer;
+        return dist[row - 1][col - 1] == Integer.MAX_VALUE 
+            ? (-1) 
+            : dist[row - 1][col - 1];
     }
 }
