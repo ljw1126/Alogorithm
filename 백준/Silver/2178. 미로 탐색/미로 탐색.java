ede8fd22cff29f1fbb22a50eefaeab2126d6cd78
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-17 11:20:54 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,117 +3,82 @@ import java.io.*;
 
 public class Main {
     
-    static StringBuilder sb = new StringBuilder();
-    static int[][] DIR = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-    static int N, M;
-    static String[] TEXT;
-    static boolean[][] VISIT;
-    static int[][] DIST;
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         input();
-
         pro();
-
         output();
     }
 
-    private static void pro() {
-        bfs(0, 0);
-        sb.append(DIST[N - 1][M - 1]);
-    }
+    private static int n, m;
+    private static int[][] matrix;
 
-    private static void bfs(int startX, int startY) {
-        Deque<Integer> que = new ArrayDeque<>();
-        que.add(startX);
-        que.add(startY);
-        DIST[startX][startY] = 1;
-        VISIT[startX][startY] = true;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while(!que.isEmpty()) {
-            int x = que.poll();
-            int y = que.poll();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-            for(int i = 0; i < 4; i++) {
-                int nx = x + DIR[i][0];
-                int ny = y + DIR[i][1];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if(TEXT[nx].charAt(ny) == '0') continue;
-                if(VISIT[nx][ny]) continue;
-
-                if(DIST[nx][ny] >= DIST[x][y] + 1) {
-                    VISIT[nx][ny] = true;
-                    DIST[nx][ny] = DIST[x][y] + 1;
-                    que.add(nx);
-                    que.add(ny);
-                }
+        matrix = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            String line = br.readLine();
+            for(int j = 1; j <= m; j++) {
+                matrix[i][j] = line.charAt(j - 1) - '0';
             }
         }
     }
 
-    private static void input() {
-        InputProcessor inputProcessor = new InputProcessor();
-        N = inputProcessor.nextInt(); // row
-        M = inputProcessor.nextInt(); // col
-
-        TEXT = new String[N];
-        for(int i = 0; i < N; i++) {
-            TEXT[i] = inputProcessor.nextLine();
+    private static void pro() {
+        int[][] dist = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
 
-        DIST = new int[N][M];
-        for(int i = 0; i < N; i++) {
-            Arrays.fill(DIST[i], Integer.MAX_VALUE);
+        int[][] dir = {
+                {0, 1},
+                {1, 0},
+                {0, -1},
+                {-1, 0}
+        };
+
+        Deque<Integer> que = new ArrayDeque<>();
+        que.add(1);
+        que.add(1);
+        
+        dist[1][1] = 1;
+
+        while(!que.isEmpty()){
+            int x = que.poll();
+            int y = que.poll();
+
+            if(x == n && y == m) {
+                continue;
+            }
+
+            for(int i = 0; i < 4; i++) {
+                int dx =  x + dir[i][0];
+                int dy =  y + dir[i][1];
+
+                if(dx < 1 || dy < 1 || dx > n || dy > m) continue;
+                if(matrix[dx][dy] == 0) continue;
+                if(dist[dx][dy] != Integer.MAX_VALUE) continue;
+
+                dist[dx][dy] = dist[x][y] + 1;
+                que.add(dx);
+                que.add(dy);
+            }
         }
 
-        VISIT = new boolean[N][M];
+        sb.append(dist[n][m]);
     }
+
     private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-
-    private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while(st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String input = "";
-
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return input;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
     }
     
 }
