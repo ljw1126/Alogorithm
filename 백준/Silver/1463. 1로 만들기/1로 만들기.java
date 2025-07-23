36e4eee3eb4dc327b9c87c23cf0d951cca15fc77
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-23 09:19:47 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -9,32 +10,36 @@ public class Main {
         pro();
         output();
     }
-    private static int x;
+
+    private static final int MAX_VALUE = 1000001;
+    private static int n;
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        x = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
     }
 
-    private static void pro() {
-        int[] dp = new int[1000001];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0;
+     private static void pro() throws IOException {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, MAX_VALUE);
 
-        for(int i = 2; i <= x; i++) {
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
+
+            if(i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
 
             if(i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            
-            if(i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
         }
 
-        sb.append(dp[x]);
+        sb.append(dp[n]);
     }
+
 
     private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
