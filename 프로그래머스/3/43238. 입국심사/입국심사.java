f8a6fcf880a16a4aa85369a24e73ecb6fe93b0a5
Lee jinwoo <leejinwoo1126@gmail.com>
2025-07-30 08:10:12 +0900

    [level 3] Title: 입국심사, Time: 124.64 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 class Solution {
-    // 시간이 t가 주어졌을때 모든 사람(n)이 심사받을 수 있는가? (최소값)
+    // 시간이 t가 주어졌을때 심사관이 검증할 수 있는 사람수가 n명 이상인가? (최소값)
+    // long 최대치는 9 * 10^18
+    // 입국 심사 인원 10^9명, 심사위원 한명이 걸리는 최대 시간 10^9일때 R은 10^18이 최대가 걸림
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
         
         long L = 1L;
-        long R = (long) times[times.length - 1] * n; 
+        long R = (long) times[times.length - 1] * n;
         while(L <= R) {
             long mid = (L + R) / 2;
             
@@ -25,6 +27,10 @@ class Solution {
     private boolean isPossible(long limit, int n, int[] times) {
         long count = 0L;
         for(int i = 0; i < times.length; i++) {
+            if(count >= Long.MAX_VALUE - (limit / times[i])) {
+                return false;
+            }
+            
             count += (limit / times[i]);
         }
         
