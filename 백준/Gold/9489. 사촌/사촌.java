3c75b41752eb5988d1d3841cffdadd78982634f1
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-06 12:32:15 +0900

    [Gold IV] Title: 사촌, Time: 680 ms, Memory: 151304 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.java
+++ 백준/Gold/9489. 사촌/사촌.java
@@ -2,47 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-   static StringBuilder sb = new StringBuilder();
+  static StringBuilder sb = new StringBuilder();
 
-    static int N, K, TARGET_IDX;
+    static int N, K, ROOT, TARGET_IDX;
 
-    static int[] DATA, PARENT;
-    
+    static int[] TREE, PARENT;
+
     static void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         String input = "";
-        while(true) {
-            input = br.readLine();
-            if("0 0".equals(input)) break;
-
+        while(!(input = br.readLine()).equals("0 0")) {
             st = new StringTokenizer(input);
+
             N = Integer.parseInt(st.nextToken()); // 노드의 수
             K = Integer.parseInt(st.nextToken()); // 사촌의 수
 
-            st = new StringTokenizer(br.readLine());
-            DATA = new int[N + 1];
+            TREE = new int[N + 1];
             PARENT = new int[N + 1];
 
-            PARENT[0] = -1;
+            st = new StringTokenizer(br.readLine());
 
             int parentIdx = 0;
-            for(int i = 1; i <= N; i++) {
-                DATA[i] = Integer.parseInt(st.nextToken());
-                if(DATA[i] == K) TARGET_IDX = i;
-                if(i > 1 && DATA[i] - DATA[i - 1] != 1) parentIdx += 1;
+            PARENT[0] = -1;
 
-                PARENT[i] = parentIdx;
+            for(int i = 1; i <= N; i++) {
+               TREE[i] = Integer.parseInt(st.nextToken());
+
+               if(TREE[i] == K) TARGET_IDX = i;
+
+               if(i > 1 && TREE[i] - TREE[i - 1] != 1) parentIdx += 1;
+
+               PARENT[i] = parentIdx;
             }
 
             pro();
         }
     }
+
     static void pro() {
         int cnt = 0;
         for(int i = 1; i <= N; i++) {
-            if(PARENT[i] != PARENT[TARGET_IDX] && PARENT[PARENT[i]] == PARENT[PARENT[TARGET_IDX]]) cnt += 1;
+            if(PARENT[TARGET_IDX] != PARENT[i] && PARENT[PARENT[TARGET_IDX]] == PARENT[PARENT[i]]) cnt += 1;
         }
 
         sb.append(cnt).append("\n");
