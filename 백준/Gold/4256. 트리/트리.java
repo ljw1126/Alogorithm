ca7478e98001cc6c80cd29ba642515b7868833e0
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-06 19:39:59 +0900

    [Gold II] Title: 트리, Time: 332 ms, Memory: 34980 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -3,59 +3,56 @@ import java.io.*;
 
 public class Main {
     
-     private static StringBuilder sb = new StringBuilder();
-    private static InputProcessor inputProcessor = new InputProcessor();
-
-    private static final String BLANK = " ";
-    private static int T, N;
-    private static int[] PRE_ORDER, IN_ORDER;
-
+    private static final StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         input();
         output();
     }
 
-    private static void input() {
-        T = inputProcessor.nextInt();
-        while (T > 0) {
-            T -= 1;
+    private static int t, n;
+    private static int[] preOrder, inOrder;
 
-            N = inputProcessor.nextInt(); // 노드의 개수
-            PRE_ORDER = new int[N + 1];
-            for (int i = 1; i <= N; i++) {
-                PRE_ORDER[i] = inputProcessor.nextInt();
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            t -= 1;
+
+            n = Integer.parseInt(br.readLine());
+            preOrder = new int[n + 1];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                preOrder[i] = Integer.parseInt(st.nextToken());
             }
 
-            IN_ORDER = new int[N + 1]; // 루트 인덱스를 쉽게 찾기 위헤
-            for (int i = 1; i <= N; i++) {
-                int node = inputProcessor.nextInt();
-                IN_ORDER[node] = i;
+            inOrder = new int[n + 1]; // 루트 인덱스를 빠르게 찾기 위해 노하우를 적용
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                int v = Integer.parseInt(st.nextToken());
+                inOrder[v] = i;
             }
 
             pro();
-            sb.append("\n");
         }
     }
 
-
     private static void pro() {
-        rec(1, N, 1, N);
+        rec(1, n, 1, n);
+        sb.append("\n");
     }
 
     private static void rec(int preS, int preE, int inS, int inE) {
-        if (preS > preE || inS > inE) {
-            return;
-        }
+        if (preS > preE || inS > inE) return;
 
-        int root = PRE_ORDER[preS];
-        int rootIdx = IN_ORDER[root];
+        int root = preOrder[preS];
+        int rootIdx = inOrder[root];
 
-        int len = rootIdx - inS;
-
-        rec(preS + 1, preS + len, inS, rootIdx - 1); // 왼쪽
-        rec(preS + len + 1, preE, rootIdx + 1, inE); // 오른쪽
-        sb.append(root).append(BLANK); // 루트
+        rec(preS + 1, (preS + 1) + (rootIdx - 1 - inS), inS, rootIdx - 1);
+        rec((preS + 1) + (rootIdx - 1 - inS) + 1, preE, rootIdx + 1, inE);
+        sb.append(root).append(" ");
     }
 
     private static void output() throws IOException {
@@ -63,45 +60,6 @@ public class Main {
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-
-    private static class InputProcessor {
-        private BufferedReader br;
-        private StringTokenizer st;
-
-        public InputProcessor() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String input = "";
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return input;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
     }
     
 }
