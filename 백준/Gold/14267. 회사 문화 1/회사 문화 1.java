132fb1734bda4115067cb46db97a467647044c5a
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-06 12:59:17 +0900

    [Gold IV] Title: 회사 문화 1, Time: 572 ms, Memory: 77492 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -2,61 +2,75 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
- static StringBuilder sb = new StringBuilder();
+    private static final StringBuilder sb = new StringBuilder();
 
-    static int N, M;
-
-    static List<Integer>[] adj;
-
-    static int[] SCORE;
-
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); // 직원 수 (노드 수)
-        M = Integer.parseInt(st.nextToken()); // 칭찬 횟수
-
-        SCORE = new int[N + 1];
-        adj = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {
-            int parent  = Integer.parseInt(st.nextToken());
-
-            if(i == 1) continue;
-
-            adj[parent].add(i);
-        }
-
-        for(int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int to = Integer.parseInt(st.nextToken());
-            int praise = Integer.parseInt(st.nextToken());
-
-            SCORE[to] += praise;
-        }
-    }
-
-    static void dfs(int node, int value) {
-        SCORE[node] += value;
-
-        for(int next : adj[node]) {
-            dfs(next, SCORE[node]);
-        }
-    }
-
-    static void pro() {
-        dfs(1, 0);
-        for(int i = 1; i <= N; i++) sb.append(SCORE[i]).append(" ");
-
-        System.out.println(sb);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
+    }
+
+    private static final int ROOT = 1;
+    private static int n, m;
+    private static long[] score;
+    private static List<List<Integer>> adj;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 회사의 직원 수
+        m = Integer.parseInt(st.nextToken()); // 최초의 칭찬의 횟수
+
+        adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int parent = Integer.parseInt(st.nextToken());
+
+            if (parent == -1) continue;
+
+            adj.get(parent).add(i);
+        }
+
+        score = new long[n + 1];
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            score[a] += v;
+        }
+    }
+
+    private static void pro() {
+        rec(ROOT, -1);
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(score[i]).append(" ");
+        }
+    }
+
+    private static void rec(int node, int prev) {
+        if (adj.get(node).isEmpty()) {
+            return;
+        }
+
+        for (int child : adj.get(node)) {
+            if (child == prev) continue;
+
+            score[child] += score[node];
+            rec(child, node);
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
