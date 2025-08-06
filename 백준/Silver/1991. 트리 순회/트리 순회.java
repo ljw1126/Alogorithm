f75ea944dbe12883c6f5bed88a6fe1fa1d625c6d
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-06 10:32:54 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -3,78 +3,88 @@ import java.io.*;
 
 public class Main {
     
-    static StringBuilder sb = new StringBuilder();
+    private static final StringBuilder sb = new StringBuilder();
 
-    static int N;
-
-    static int[][] tree;
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-
-        tree = new int[27][2];
-
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int parent = st.nextToken().charAt(0) - 'A';
-
-            for(int j = 0; j < 2; j++) {
-                char child = st.nextToken().charAt(0);
-
-                if(child != '.') tree[parent][j] = (child - 'A');
-                else tree[parent][j] = -1;
-            }
-        }
-
-    }
-
-    // 전위 순회 - 루트, 왼쪽, 오른쪽
-    static void preorder(int node) {
-        if(node == -1) return;
-
-        sb.append((char)(node + 'A'));
-        preorder(tree[node][0]);
-        preorder(tree[node][1]);
-    }
-
-    // 중위 순회 - 왼쪽, 루트, 오른쪽
-    static void inorder(int node) {
-        if(node == -1) return;
-
-        inorder(tree[node][0]);
-        sb.append((char)(node + 'A'));
-        inorder(tree[node][1]);
-    }
-
-    // 후위 순회 - 왼쪽, 오른족, 루트
-    static void postorder(int node) {
-        if(node == -1) return;
-
-        postorder(tree[node][0]);
-        postorder(tree[node][1]);
-        sb.append((char)(node + 'A'));
-    }
-
-    static void pro() {
-        int rootNode = 0;
-
-        preorder(rootNode);
-        sb.append("\n");
-
-        inorder(rootNode);
-        sb.append("\n");
-
-        postorder(rootNode);
-        sb.append("\n");
-
-        System.out.println(sb);
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
+        output();
+    }
+
+    private static int n;
+    private static int[][] alphabets;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        alphabets = new int[27][2]; // 0 : 왼쪽, 1: 오른쪽
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int parent = toInt(st.nextToken());
+            int left = toInt(st.nextToken());
+            int right = toInt(st.nextToken());
+
+            alphabets[parent][0] = left;
+            alphabets[parent][1] = right;
+        }
+    }
+
+    private static int toInt(String s) {
+        if (s.equals(".")) return -1;
+
+        return s.charAt(0) - 'A';
+    }
+
+    private static void pro() {
+        int root = 0; // 'A';
+
+        preOrder(root);
+        sb.append("\n");
+
+        inOrder(root);
+        sb.append("\n");
+
+        postOrder(root);
+    }
+
+    private static char toChar(int node) {
+        return (char) (node + 'A');
+    }
+
+    // 전위 순회 - 루트 > 왼쪽 > 오른쪽
+    private static void preOrder(int node) {
+        if (node == -1) return;
+
+        sb.append(toChar(node));
+        preOrder(alphabets[node][0]);
+        preOrder(alphabets[node][1]);
+    }
+
+    // 중위 순회
+    private static void inOrder(int node) {
+        if (node == -1) return;
+
+        inOrder(alphabets[node][0]);
+        sb.append(toChar(node));
+        inOrder(alphabets[node][1]);
+    }
+
+    // 후위 순회
+    private static void postOrder(int node) {
+        if (node == -1) return;
+
+        postOrder(alphabets[node][0]);
+        postOrder(alphabets[node][1]);
+        sb.append(toChar(node));
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
 }
