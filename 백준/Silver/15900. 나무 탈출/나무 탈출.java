770638868fecf910ba5ab274f5c1e39847e26633
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-06 11:23:09 +0900

    [Silver I] Title: 나무 탈출, Time: 1748 ms, Memory: 212448 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -3,52 +3,70 @@ import java.io.*;
 
 public class Main {
     
-   static int N, ANS;
-    static List<Integer>[] tree;
+    private static final StringBuilder sb = new StringBuilder();
 
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-
-        tree = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++) tree[i] = new ArrayList();
-
-        for(int i = 1; i <= (N - 1); i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-
-            tree[from].add(to);
-            tree[to].add(from);
-        }
-
-        br.close();
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
     }
 
-    static void dfs(int node, int parent, int depth) {
-        if(node != 1 && tree[node].size() == 1 && tree[node].get(0) == parent) {
-            ANS += depth;
+    private static final int ROOT = 1;
+    private static List<List<Integer>> adj;
+    private static int n;
+    private static int[] depths;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+
+        depths = new int[n + 1];
+    }
+
+    private static void pro() {
+        rec(ROOT, -1, 0);
+
+        if (depths[ROOT] % 2 == 0) {
+            sb.append("No");
+        } else {
+            sb.append("Yes");
+        }
+    }
+
+    private static void rec(int node, int parent, int depth) {
+        List<Integer> childs = adj.get(node);
+        if (childs.size() == 1 && childs.get(0) == parent) {
+            depths[node] = depth;
             return;
         }
 
-        for(int child : tree[node]) {
-            if(child == parent) continue;
+        for (int next : childs) {
+            if (next == parent) continue;
 
-            dfs(child, node,depth + 1);
+            rec(next, node, depth + 1);
+            depths[node] += depths[next];
         }
     }
 
-    static void pro() {
-        dfs(1, -1,  0);
-
-        System.out.println(ANS % 2 == 1 ? "Yes" : "No");
-    }
-
-    public static void main(String[] args) throws Exception {
-        input();
-        pro();
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
 }
