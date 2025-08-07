36227281d47488deb212853359a03d5bc7c5d3a5
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-07 14:35:34 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -3,29 +3,31 @@ import java.io.*;
 
 public class Main {
     
-    static StringBuilder sb = new StringBuilder();
-    static InputProcessor inputProcessor = new InputProcessor();
-    static int N;
-    static int[] DP;
+   private static final StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        N = inputProcessor.nextInt();
-
+        input();
         pro();
-        sb.append(DP[N]);
-
         output();
     }
 
+    private static int n;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+    }
+
     private static void pro() {
-        DP = new int[N + 1];
-        if(N >= 1) {
-            DP[1] = 1;
+        int[] dp = new int[21];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= 20; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        for(int i = 2; i <= N; i++) {
-            DP[i] = DP[i - 1] + DP[i - 2];
-        }
+        sb.append(dp[n]);
     }
 
     private static void output() throws IOException {
@@ -33,47 +35,6 @@ public class Main {
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-
-    private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while(st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String input = "";
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return input;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
     }
     
 }
