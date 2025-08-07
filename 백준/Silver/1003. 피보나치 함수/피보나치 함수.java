8626db17de096a5fa1a63d909c93e155dcc31870
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-07 14:30:11 +0900

    [Silver III] Title: 피보나치 함수, Time: 88 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,39 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    
+    private static final StringBuilder sb = new StringBuilder();
 
-    static StringBuilder sb = new StringBuilder();
-
-    static int T, N;
-
-    static int[] DP;
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-
-        while(T > 0) {
-            T -= 1;
-
-            N = Integer.parseInt(br.readLine());
-            
-            if(N == 0) sb.append(1).append(" ").append(0).append("\n");
-            else sb.append(DP[N - 1]).append(" ").append(DP[N]).append("\n");
-        }
-    }
-
-    static void fibonacci() {
-        DP = new int[41];
-        DP[0] = 0;
-        DP[1] = 1;
-        for(int i = 2; i <= 40; i++) {
-            DP[i] = DP[i - 1] + DP[i - 2];
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        fibonacci();
+    public static void main(String[] args) throws IOException {
+        preprocess();
         input();
-        System.out.println(sb);
+        output();
+    }
+
+    private static int n;
+    private static int[][] dp;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            t -= 1;
+
+            n = Integer.parseInt(br.readLine());
+
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+
+    }
+
+    private static void preprocess() {
+        dp = new int[41][2]; // 0 : 0번 호출, 1번 호출
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
 }
