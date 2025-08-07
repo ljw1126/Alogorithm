acd2342704c5cc1c4f9d0aa2bddbf4d930b40b29
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-07 18:22:47 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 176 ms, Memory: 21804 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -2,69 +2,72 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
-    static String input;
-    static int TEXT_LENGTH;
+    private static StringBuilder sb = new StringBuilder();
 
-    static int[] alphabet, DP;
-    static boolean[][] PALINDROME;
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
+    }
 
-    static void input() throws Exception {
+    private static String text;
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        input = br.readLine();
-        TEXT_LENGTH = input.length();
-
-        alphabet = new int[TEXT_LENGTH + 1];
-        for(int i = 1; i <= TEXT_LENGTH; i++) {
-            alphabet[i] = input.charAt(i - 1) - 'A';
-        }
+        text = br.readLine();
     }
 
-    static void fulfillPalindrome() {
-        PALINDROME = new boolean[TEXT_LENGTH + 1][TEXT_LENGTH + 1];
-
-        for(int i = 1; i <= TEXT_LENGTH; i++) {
-            PALINDROME[i][i] = true;
+    private static void pro() {
+        int length = text.length();
+        char[] arr = new char[length + 1];
+        for(int i = 1; i <= length; i++){
+            arr[i] = text.charAt(i - 1);
         }
 
-        for(int i = 1; i < TEXT_LENGTH; i++) {
-            if(alphabet[i] == alphabet[i + 1]) PALINDROME[i][i + 1] = true;
+        boolean[][] phallendrome = new boolean[length + 1][length + 1];
+        for(int i = 1; i <= length; i++) {
+            phallendrome[i][i] = true;
         }
 
-        for(int len = 3; len <= TEXT_LENGTH; len++) {
-            for(int i = 1; i <= (TEXT_LENGTH - len + 1); i++) {
-                int j = i - 1 + len;
-                if(alphabet[i] == alphabet[j] && PALINDROME[i + 1][j - 1]) {
-                    PALINDROME[i][j] = true;
-                }
+        for(int i = 1; i <= length - 1; i++) {
+            if(arr[i] == arr[i + 1]) {
+                phallendrome[i][i + 1] = true;
             }
         }
-    }
 
-    static void pro() {
-        DP = new int[TEXT_LENGTH + 1];
+        for(int len = 3; len <= length; len++) {
+            for(int i = 1; i <= length - len + 1; i++) {
+                int j = i + len - 1;
 
-        Arrays.fill(DP, Integer.MAX_VALUE);
-        DP[0] = 0;
-
-        for(int i = 1; i <= TEXT_LENGTH; i++) {
-            for(int j = 1; j <= i; j++) {
-                if(PALINDROME[j][i]) {
-                    DP[i] = Math.min(DP[i], DP[j - 1] + 1);
+                if(arr[i] == arr[j] && phallendrome[i + 1][j - 1]) {
+                    phallendrome[i][j] = true;
                 }
             }
         }
 
-        System.out.println(DP[TEXT_LENGTH]);
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= length; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            for(int j = 1; j < i; j++) {
+                if(phallendrome[j][i]) {
+                    dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                }
+            }
+        }
+
+        sb.append(dp[length]);
     }
 
-    public static void main(String[] args) throws Exception {
-        input();
-
-        fulfillPalindrome();
-
-        pro();
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
 }
