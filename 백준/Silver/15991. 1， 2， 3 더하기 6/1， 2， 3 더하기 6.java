8b10937b3dbfbb15d25d7092de0beedc602a4bf1
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-07 15:10:59 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 144 ms, Memory: 20780 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
@@ -2,42 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
-    static StringBuilder sb = new StringBuilder();
-    static InputProcessor inputProcessor = new InputProcessor();
-    static String NEW_LINE = System.lineSeparator();
-    static int MOD = 1000000009;
-    
-    static int T, N;
-    static long[] DP = new long[100001];
+    private static final StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        preprocess();
-
-        T = inputProcessor.nextInt();
-        while(T > 0) {
-            N = inputProcessor.nextInt();
-
-            sb.append(DP[N]).append(NEW_LINE);
-
-            T -= 1;
-        }
-
+        input();
         output();
     }
 
-    private static void preprocess() {
-        // 초기화
-        DP[1] = 1; // 1
-        DP[2] = 2; // 1 + 1, 2
-        DP[3] = 2; // 1 + 1 + 1, 3
-        DP[4] = 3; // 1 + 1 + 1 + 1, 1 + 2 + 1, 2 + 2
-        DP[5] = 3; // 1 + 1 + 1 + 1 + 1, 1 + 3 + 1, 2 + 1 + 2
-        DP[6] = 6; // 1 + DP[4] + 1, 2 + DP[2] + 3, 3 + 3
+    private static final int MOD = 1000_000_009;
+    private static int n;
+    private static long[] dp;
 
-        for(int i = 7; i <= 100000; i++) {
-            DP[i] = (DP[i - 2] + DP[i - 4] + DP[i - 6]) % MOD;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        dp = new long[100001];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 2;
+        dp[4] = 3;
+        dp[5] = 3;
+        dp[6] = 6;
+
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            t -= 1;
+
+            n = Integer.parseInt(br.readLine());
+
+            sb.append(topDown(n)).append("\n");
         }
+    }
+
+    // 양쪽에 1을 추가하는 경우, 2를 추가하는 경우, 3을 추가하는 경우
+    private static long topDown(int node) {
+        if (dp[node] != -1) return dp[node];
+
+        dp[node] = (topDown(node - 2) + topDown(node - 4) + topDown(node - 6)) % MOD;
+
+        return dp[node];
     }
 
     private static void output() throws IOException {
@@ -45,47 +50,6 @@ public class Main {
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-
-    private static class InputProcessor {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while(st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String input = "";
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return input;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
     }
     
 }
