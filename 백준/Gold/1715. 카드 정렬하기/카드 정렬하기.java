946065cdbfac7181958e35e0da3edaf68d2e4f86
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-09 09:47:29 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 332 ms, Memory: 26704 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -2,96 +2,53 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
     private static StringBuilder sb = new StringBuilder();
-    private static InputProcessor inputProcessor = new InputProcessor();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
         output();
     }
 
     private static int n;
-    private static int[] data;
+    private static long[] data;
 
-    private static void input() {
-        n = inputProcessor.nextInt();
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
-        data = new int[n];
+        data = new long[n];
         for(int i = 0; i < n; i++) {
-            data[i] = inputProcessor.nextInt();
+            data[i] = Long.parseLong(br.readLine());
         }
     }
 
     private static void pro() {
-        Queue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < n; i++) {
+        Queue<Long> pq = new PriorityQueue<>();
+        for(int i = 0; i < n; i++)  {
             pq.add(data[i]);
         }
 
-        int result = 0;
+        long result = 0;
         while(!pq.isEmpty()) {
             if(pq.size() < 2) break;
-            int v1 = pq.poll();
-            int v2 = pq.poll();
 
-            int sum = v1 + v2;
+            long a = pq.poll();
+            long b = pq.poll();
+            long sum = a + b;
             result += sum;
+            
             pq.add(sum);
         }
-
+        
         sb.append(result);
     }
 
-    private static void output() {
-        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            bw.write(sb.toString());
-            bw.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
-    private static class InputProcessor {
-        private BufferedReader br;
-        private StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String result = "";
-
-            try {
-                result = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return result;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
-    
 }
