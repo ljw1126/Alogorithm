bb0d6caa6ff9928d72a50815707ee59fcab59d40
Lee jinwoo <leejinwoo1126@gmail.com>
2025-08-19 10:20:14 +0900

    [Gold IV] Title: 플로이드, Time: 364 ms, Memory: 42524 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -2,106 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
-    private static StringBuilder sb = new StringBuilder();
-    private static InputProcessor inputProcessor = new InputProcessor();
+   private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
         pro();
         output();
     }
 
-    private static final int INF = 10_000_001;
+    private static final int MAX_VALUE = 10000001;
     private static int n, m;
-    private static int[][] dist;
-    
-    private static void input() {
-        n = inputProcessor.nextInt(); // 도시의 개수(노드)
-        m = inputProcessor.nextInt(); // 버스의 개수(간선)
+    private static int[][] floyed;
 
-        dist = new int[n + 1][n + 1];
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        floyed = new int[n + 1][n + 1];
         for(int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
+            Arrays.fill(floyed[i], MAX_VALUE);
+            floyed[i][i] = 0;
         }
 
-        for(int i = 1; i <= m; i++) {
-            int a = inputProcessor.nextInt();
-            int b = inputProcessor.nextInt();
-            int c = inputProcessor.nextInt();
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            dist[a][b] = Math.min(dist[a][b], c);
+            floyed[a][b] = Math.min(floyed[a][b], c);
         }
     }
 
     private static void pro() {
         for(int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (i == j) continue;
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(i == j) continue;
 
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    floyed[i][j] = Math.min(floyed[i][j], floyed[i][k] + floyed[k][j]);
                 }
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if(dist[i][j] == INF) sb.append(0).append(" ");
-                else sb.append(dist[i][j]).append(" ");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(floyed[i][j] == MAX_VALUE) sb.append(0).append(" ");
+                else sb.append(floyed[i][j]).append(" ");
             }
+
             sb.append("\n");
         }
     }
 
-    private static void output() {
-        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            bw.write(sb.toString());
-            bw.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static class InputProcessor {
-        private BufferedReader br;
-        private StringTokenizer st;
-
-        public InputProcessor() {
-            this.br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return st.nextToken();
-        }
-
-        public String nextLine() {
-            String result = "";
-
-            try {
-                result = br.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return result;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
 }
